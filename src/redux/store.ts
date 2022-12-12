14409493ff947ea43671472a49f5647ce41cec63
nhkhang2002 <khang.nguyen@journeyh.io>
2022-12-12 11:34:28 +0700

    update:  disable serializableCheck for redux-toolkit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -12,6 +12,7 @@ export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
+      serializableCheck: false,
       thunk: { extraArgument: sdk },
     }),
 });
