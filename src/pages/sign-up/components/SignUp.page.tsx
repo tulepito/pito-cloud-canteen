e7a75676d100719aa2afb703b6833a0ae771380a
nhkhang2002 <khang.nguyen@journeyh.io>
2022-12-12 09:58:12 +0700

    update: width for formContainer (SignUpForm)

--- src/pages/sign-up/components/SignUp.page.tsx
+++ src/pages/sign-up/components/SignUp.page.tsx
@@ -1,11 +1,8 @@
-import { FormattedMessage } from 'react-intl';
-
 import SignUpForm from './SignUpForm';
 
 const SignUpPage = () => {
   return (
     <div>
-      <FormattedMessage id="SignUpPage.Title" />
       <SignUpForm onSubmit={() => {}} />
     </div>
   );
