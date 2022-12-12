78774bb1557f75bc9697841f7440e6db20a146d8
nhkhang2002 <khang.nguyen@journeyh.io>
2022-12-12 16:53:23 +0700

    update: apply function for sign in page

--- src/pages/dang-nhap/components/SignIn.page.tsx
+++ src/pages/dang-nhap/components/SignIn.page.tsx
@@ -1,14 +1,41 @@
+import { authenticationInProgress, authThunks } from '@redux/slices/auth.slice';
+import type { AppDispatch, RootState } from '@redux/store';
+import { FormattedMessage } from 'react-intl';
+import { useDispatch, useSelector } from 'react-redux';
+
 import SignInForm from './SignInForm';
 
-const SignUpPage = () => {
+const SignInPage = () => {
+  const appState = useSelector((state: RootState) => state);
+  const authInprogress = authenticationInProgress(appState);
+  const { loginError } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const signInErrorMessage = loginError ? (
+    <>
+      <div>
+        <FormattedMessage id="SignInPage.loginFailedText1" />
+      </div>
+      <div>
+        <FormattedMessage id="SignInPage.loginFailedText2" />
+      </div>
+    </>
+  ) : undefined;
+
+  const handleSubmitSignUp = (values: Record<string, any>) => {
+    const { email, password } = values;
+
+    dispatch(authThunks.login({ email, password }));
+  };
+
   return (
     <div>
       <SignInForm
-        onSubmit={() => {}}
-        errorMessage={undefined}
-        inProgress={true}
+        onSubmit={handleSubmitSignUp}
+        errorMessage={signInErrorMessage}
+        inProgress={authInprogress}
       />
     </div>
   );
 };
-export default SignUpPage;
+export default SignInPage;
