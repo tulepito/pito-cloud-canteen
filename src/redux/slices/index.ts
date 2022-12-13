b88af5be000860382df6e5c028ac0c25e5caf991
nhkhang2002 <khang.nguyen@journeyh.io>
2022-12-13 17:47:07 +0700

    add: create passwordRecoverySlice

--- src/redux/slices/index.ts
+++ src/redux/slices/index.ts
@@ -1,2 +1,3 @@
 export { default as auth } from './auth.slice';
+export { default as passwordRecovery } from './passwordRecovery.slice';
 export { default as user } from './user.slice';
---
+++ src/redux/slices/passwordRecovery.slice.ts
@@ -0,0 +1,87 @@
+import type { ThunkAPI } from '@redux/store';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { storableError } from '@utils/errors';
+
+// ================ Thunk types ================ //
+const AUTH_INFO = 'app/PasswordRecovery';
+
+type TPasswordRecoverySliceInitialState = {
+  initialEmail: string | null;
+  submittedEmail: string | null;
+  recoveryError: any;
+  recoveryInProgress: Boolean;
+  passwordRequested: Boolean;
+};
+
+const initialState: TPasswordRecoverySliceInitialState = {
+  initialEmail: null,
+  submittedEmail: null,
+  recoveryError: null,
+  recoveryInProgress: false,
+  passwordRequested: false,
+};
+
+export const recoverPassword = createAsyncThunk(
+  AUTH_INFO,
+  async (
+    params: Record<string, any>,
+    { extra: sdk, fulfillWithValue, rejectWithValue }: ThunkAPI,
+  ) => {
+    const { email } = params;
+
+    try {
+      sdk.passwordReset.request({ email });
+      return fulfillWithValue({ email });
+    } catch (error) {
+      return rejectWithValue({ error: storableError(error), email });
+    }
+  },
+);
+
+const passwordRecoverySlice = createSlice({
+  name: 'PasswordRecovery',
+  initialState,
+  reducers: {
+    retypePasswordRecoveryEmail: (state) => {
+      return {
+        ...state,
+        initialEmail: state.submittedEmail,
+        submittedEmail: null,
+        passwordRequested: false,
+      };
+    },
+    clearPasswordRecoveryError: (state) => {
+      return { ...state, recoveryError: null };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(recoverPassword.pending, (state) => {
+        return {
+          ...state,
+          submittedEmail: null,
+          recoveryInProgress: true,
+          recoveryError: null,
+        };
+      })
+      .addCase(recoverPassword.fulfilled, (state, action) => {
+        return {
+          ...state,
+          submittedEmail: action.payload.email,
+          initialEmail: action.payload.email,
+          recoveryInProgress: false,
+          passwordRequested: true,
+        };
+      })
+      .addCase(recoverPassword.rejected, (state, { payload }: any) => {
+        return {
+          ...state,
+          recoveryInProgress: false,
+          recoveryError: payload.error,
+          initialEmail: payload.email,
+        };
+      });
+  },
+});
+
+export default passwordRecoverySlice.reducer;
