64cdb42612efefcd64489acef1ac2e692ba70c85
nhkhang2002 <khang.nguyen@journeyh.io>
2022-12-15 11:09:33 +0700

    update: splitNameFormFullName util

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,7 +1,8 @@
 export const splitNameFormFullName = (fullName: string) => {
   const firstSpaceIndx = fullName.split('').findIndex((ch) => ch === ' ');
+
   if (firstSpaceIndx === -1) {
-    return { firstName: fullName };
+    return { firstName: fullName, lastName: fullName };
   }
 
   return {
