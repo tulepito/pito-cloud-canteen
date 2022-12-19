0dc2b040c3fbcfae87e4331f8fe21ec2eb05f852
nhkhang2002 <khang.nguyen@journeyh.io>
2022-12-19 15:06:19 +0700

    update: disable condition of log out button

--- src/pages/index.route.tsx
+++ src/pages/index.route.tsx
@@ -1,12 +1,14 @@
 import Button from '@components/Button/Button';
 import { useAppDispatch, useAppSelector } from '@redux/reduxHooks';
-import { authThunks } from '@redux/slices/auth.slice';
+import { authenticationInProgress, authThunks } from '@redux/slices/auth.slice';
 import { useRouter } from 'next/router';
 
 import css from './index.module.scss';
 
 export default function Home() {
-  const { logoutInProgress } = useAppSelector((state) => state.auth);
+  const inProgress = useAppSelector(authenticationInProgress);
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
+
   const router = useRouter();
   const dispatch = useAppDispatch();
 
@@ -18,7 +20,10 @@ export default function Home() {
   return (
     <div className={css.root}>
       <h1>Trang chủ</h1>
-      <Button onClick={handleLogout} inProgress={logoutInProgress}>
+      <Button
+        onClick={handleLogout}
+        inProgress={inProgress}
+        disabled={inProgress || !isAuthenticated}>
         Đăng xuất
       </Button>
     </div>
