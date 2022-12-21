2fea863cf46209130b243d36e299bfe0a53153f7
ChuongNguyenJH <chuong.nguyen@journeyh.io>
2022-12-21 15:28:02 +0700

    update basic auth logic

--- src/middleware.api.ts
+++ src/middleware.api.ts
@@ -4,6 +4,7 @@ import { NextResponse } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
   const dev = process.env.NODE_ENV === 'development';
+
   if (!dev) {
     const USERNAME = process.env.BASIC_AUTH_USERNAME;
     const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
@@ -13,11 +14,9 @@ export function middleware(req: NextRequest) {
     // If BASIC_AUTH_USERNAME and BASIC_AUTH_PASSWORD have been set - let's use them
     if (hasUsername && hasPassword) {
       const basicAuth = req.headers.get('authorization');
-
       if (basicAuth) {
         const auth = basicAuth.split(' ')[1];
         const [username, password] = atob(auth).split(':');
-
         if (!username || !password) {
           throw new Error(
             'Missing required username and password for basic authentication.',
@@ -28,18 +27,13 @@ export function middleware(req: NextRequest) {
           return NextResponse.next();
         }
       }
-      return NextResponse.rewrite(`${req.nextUrl.origin}`, {
-        status: 401,
-        headers: {
-          'WWW-Authenticate': 'Basic realm="Secure Area"',
-        },
-      });
+      const url = req.nextUrl;
+      url.pathname = '/api/auth/basic-auth';
+      return NextResponse.rewrite(url);
     }
   } else {
     return NextResponse.next();
   }
-
-  return NextResponse.next();
 }
 
 export const config = {
