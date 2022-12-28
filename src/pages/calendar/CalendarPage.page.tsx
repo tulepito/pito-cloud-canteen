f0d9b8a1983e1610557c9696942dc2168e479669
Tuan Chu <chanthientukk9@gmail.com>
2022-12-28 10:26:17 +0700

    Add example when using renderEvent

--- src/pages/calendar/CalendarPage.page.tsx
+++ src/pages/calendar/CalendarPage.page.tsx
@@ -1,6 +1,7 @@
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import CalendarDashboard from '@components/CalendarDashboard/CalendarDashboard';
+import EventTile from '@components/CalendarDashboard/components/EventTile/EventTile';
 import { EVENTS_MOCKUP } from '@components/CalendarDashboard/helpers/mockupData';
 
 import css from './CalendarPage.module.scss';
@@ -8,7 +9,7 @@ import css from './CalendarPage.module.scss';
 function CalendarPage() {
   return (
     <div className={css.root}>
-      <CalendarDashboard events={EVENTS_MOCKUP} />
+      <CalendarDashboard events={EVENTS_MOCKUP} renderEvent={EventTile} />
     </div>
   );
 }
