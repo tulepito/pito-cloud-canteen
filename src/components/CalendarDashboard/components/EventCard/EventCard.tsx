58996859c3b8e31bc310768a7e394790e1108ebe
Tuan Chu <chanthientukk9@gmail.com>
2022-12-29 11:40:20 +0700

    add popup to select food and handle ux when interact with event card

--- src/components/CalendarDashboard/components/EventCard/EventCard.tsx
+++ src/components/CalendarDashboard/components/EventCard/EventCard.tsx
@@ -1,14 +1,13 @@
-import IconClock from '@components/Icons/IconClock';
-import IconLocation from '@components/Icons/IconLocation';
-import IconShop from '@components/Icons/IconShop';
+import Tooltip from '@components/Tooltip/Tooltip';
 import classNames from 'classnames';
 import { DateTime } from 'luxon';
 import type { Event } from 'react-big-calendar';
-import { FormattedMessage } from 'react-intl';
 
 import { EVENT_STATUS } from '../../helpers/constant';
 import css from './EventCard.module.scss';
-import EventCardContentItem from './EventCardContentItem';
+import EventCardContentItems from './EventCardContentItems';
+import EventCardHeader from './EventCardHeader';
+import EventCardPopup from './EventCardPopup';
 import EventCardStatus from './EventCardStatus';
 
 export type TEventCardProps = {
@@ -18,12 +17,7 @@ export type TEventCardProps = {
 
 const EventCard: React.FC<TEventCardProps> = ({ event, index }) => {
   const status = event.resource?.status;
-  const mealType = event.resource?.type;
-  const startTime = event.start
-    ? DateTime.fromJSDate(event.start).toLocaleString(DateTime.TIME_24_SIMPLE)
-    : null;
-  const deliAddressObj = event.resource?.deliveryAddress || {};
-  const restaurantObj = event.resource?.restaurant || {};
+
   const expiredTime = event.resource?.expiredTime as Date;
   const remainTime = DateTime.fromJSDate(new Date()).diff(
     DateTime.fromJSDate(expiredTime),
@@ -32,54 +26,30 @@ const EventCard: React.FC<TEventCardProps> = ({ event, index }) => {
   const remainHours = remainTime.get('hour');
   const remainMinutes = remainTime.get('minute');
   const isExpired = remainHours > 0 && remainMinutes > 0;
+  const eventStatus = isExpired ? EVENT_STATUS.EXPIRED_STATUS : status;
 
   return (
-    <div
-      className={classNames(css.root, {
-        [css.rootExpired]: isExpired,
-        [css.rootBlue]: !isExpired && index % 2 === 1,
-        [css.rootOrange]: !isExpired && index % 2 === 0,
-      })}>
-      <div className={css.eventCardHeader}>
-        <div className={css.eventCardTitleWrapper}>
-          <div className={css.eventTitle}>#{event.title}</div>
-          <div className={css.eventTime}>{startTime}</div>
-        </div>
-        <div className={css.mealType}>
-          <FormattedMessage id={`EventCard.mealType.${mealType}`} />
+    <Tooltip
+      overlayClassName={css.tooltipOverlay}
+      tooltipContent={
+        !isExpired && <EventCardPopup event={event} status={eventStatus} />
+      }
+      placement="rightTop"
+      trigger="click"
+      overlayInnerStyle={{ backgroundColor: '#fff' }}>
+      <div
+        className={classNames(css.root, {
+          [css.rootExpired]: isExpired,
+          [css.rootBlue]: !isExpired && index % 2 === 1,
+          [css.rootOrange]: !isExpired && index % 2 === 0,
+        })}>
+        <EventCardHeader event={event} />
+        <div className={css.eventCardContentWrapper}>
+          <EventCardStatus className={css.cardStatus} status={eventStatus} />
+          <EventCardContentItems event={event} />
         </div>
       </div>
-      <div className={css.eventCardContentWrapper}>
-        <EventCardStatus
-          status={isExpired ? EVENT_STATUS.EXPIRED_STATUS : status}
-        />
-        {!isExpired && (
-          <EventCardContentItem icon={<IconClock />}>
-            <FormattedMessage
-              id="EventCard.remainTime"
-              values={{
-                hour: Math.abs(remainHours),
-                minute: Math.abs(remainMinutes),
-              }}
-            />
-          </EventCardContentItem>
-        )}
-        <EventCardContentItem icon={<IconLocation />}>
-          <FormattedMessage
-            id="EventCard.deliveryAddress"
-            values={{
-              address: deliAddressObj.address,
-              ward: deliAddressObj.ward,
-              district: deliAddressObj.district,
-              city: deliAddressObj.city,
-            }}
-          />
-        </EventCardContentItem>
-        <EventCardContentItem icon={<IconShop />}>
-          {restaurantObj.name}
-        </EventCardContentItem>
-      </div>
-    </div>
+    </Tooltip>
   );
 };
 
---
+++ src/components/CalendarDashboard/components/EventCard/api.helpers.ts
@@ -0,0 +1,4 @@
+export const submitDishSelection = (values: any, reject?: boolean) => {
+  // Update data here
+  console.log('values', values, reject);
+};
