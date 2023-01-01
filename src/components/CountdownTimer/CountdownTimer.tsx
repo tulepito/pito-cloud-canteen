b82273d30514b78e0fa927354ffea777f9542f69
Tuan Chu <chanthientukk9@gmail.com>
2023-01-01 11:03:24 +0700

    Feat: add stopAt feature for countdown and example

--- src/components/CountdownTimer/CountdownTimer.tsx
+++ src/components/CountdownTimer/CountdownTimer.tsx
@@ -2,64 +2,64 @@ import classNames from 'classnames';
 import type { Duration } from 'luxon';
 import { DateTime } from 'luxon';
 import { useEffect, useState } from 'react';
-import { FormattedMessage } from 'react-intl';
 
+import CountdownItem from './CountdownItem';
 import css from './CountdownTimer.module.scss';
+import { getStopTime } from './helpers';
+
+type TStopAt = {
+  day: number;
+  hour: number;
+  minute: number;
+  second: number;
+};
 
 type TCountdownTimerProps = {
   className?: string;
   deadline: number;
+  stopAt?: 0 | TStopAt;
 };
 
 const CountdownTimer: React.FC<TCountdownTimerProps> = ({
   className,
   deadline,
+  stopAt,
 }) => {
   const [diffTime, setDiffTime] = useState<Duration | null>(null);
 
   useEffect(() => {
+    const stopTime = getStopTime(stopAt);
     const intervalId = setInterval(() => {
       const diffObj = DateTime.fromMillis(parseInt(`${deadline}`, 10)).diffNow([
         'day',
         'hour',
         'minute',
         'second',
-        'millisecond',
       ]);
+      if (stopTime) {
+        if (
+          diffObj.get('day') <= stopTime.day &&
+          diffObj.get('hour') <= stopTime.hour &&
+          diffObj.get('minute') <= stopTime.minute &&
+          diffObj.get('second') <= stopTime.second
+        ) {
+          clearInterval(intervalId);
+        }
+      }
       setDiffTime(diffObj);
     }, 1000);
 
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, [stopAt, deadline]);
 
   return (
     <div className={classNames(css.root, className)}>
-      <div className={css.boxItem}>
-        <div className={css.value}>{diffTime?.get('day')}</div>
-        <div className={css.label}>
-          <FormattedMessage id="CountDownTimer.day" />
-        </div>
-      </div>
-      <div className={css.boxItem}>
-        <div className={css.value}>{diffTime?.get('hour')}</div>
-        <div className={css.label}>
-          <FormattedMessage id="CountDownTimer.hour" />
-        </div>
-      </div>
-      <div className={css.boxItem}>
-        <div className={css.value}>{diffTime?.get('minute')}</div>
-        <div className={css.label}>
-          <FormattedMessage id="CountDownTimer.minute" />
-        </div>
-      </div>
-      <div className={css.boxItem}>
-        <div className={css.value}>{diffTime?.get('second')}</div>
-        <div className={css.label}>
-          <FormattedMessage id="CountDownTimer.second" />
-        </div>
-      </div>
+      <CountdownItem type="day" diffTime={diffTime} />
+      <CountdownItem type="hour" diffTime={diffTime} />
+      <CountdownItem type="minute" diffTime={diffTime} />
+      <CountdownItem type="second" diffTime={diffTime} />
     </div>
   );
 };
--- src/pages/paricipant/[orderId]/OrderManagement.tsx
+++ src/pages/paricipant/[orderId]/OrderManagement.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const OrderManagement = () => {
   const router = useRouter();
   const { orderId } = router.query;
-
+  console.log('orderId', orderId);
   return <div>OrderManagement</div>;
 };
 
