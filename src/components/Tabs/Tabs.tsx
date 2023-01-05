1a1af1bdbfb52fdf6f42e09ef8cab1c18c69a657
minhtranjh <minh.tran@journeyh.io>
2023-01-05 09:31:40 +0700

    update tab component

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -22,6 +22,8 @@ interface ITabsProps {
   showNavigation?: boolean;
   actionsClassName?: string;
   actionsComponent?: ReactNode;
+  headerWrapperClassName?: string;
+  className?: string;
 }
 
 const Tabs = (props: ITabsProps) => {
@@ -34,6 +36,8 @@ const Tabs = (props: ITabsProps) => {
     onChange = () => null,
     showNavigation = false,
     actionsComponent,
+    headerWrapperClassName,
+    className,
   } = props;
   const [activeTabKey, setActiveTabKey] = useState(defaultActiveKey || 1);
 
@@ -94,21 +98,29 @@ const Tabs = (props: ITabsProps) => {
   const headerClasses = classNames(css.tabHeaders, headerClassName);
   const actionsClasses = classNames(css.actions, actionsClassName);
   const contentClasses = classNames(css.tabPanel, contentClassName);
+  const headerWrapperClasses = classNames(
+    css.headerWrapper,
+    headerWrapperClassName,
+  );
+  const classes = classNames(css.root, className);
+
   return (
-    <div className={css.root}>
-      <div className={headerClasses}>{tabHeader}</div>
+    <div className={classes}>
+      <div className={headerWrapperClasses}>
+        <div className={headerClasses}>{tabHeader}</div>
+        {showNavigation && (
+          <div className={css.navigateBtn}>
+            <InlineTextButton type="button" onClick={goLeft}>
+              <IconArrow direction="left" />
+            </InlineTextButton>
+            <InlineTextButton type="button" onClick={goRight}>
+              <IconArrow direction="right" />
+            </InlineTextButton>
+          </div>
+        )}
+      </div>
       <div className={actionsClasses}>{actionsComponent}</div>
       <div className={contentClasses}>{tabContent}</div>
-      {showNavigation && (
-        <div className={css.navigateBtn}>
-          <InlineTextButton type="button" onClick={goLeft}>
-            <IconArrow direction="left" />
-          </InlineTextButton>
-          <InlineTextButton type="button" onClick={goRight}>
-            <IconArrow direction="right" />
-          </InlineTextButton>
-        </div>
-      )}
     </div>
   );
 };
