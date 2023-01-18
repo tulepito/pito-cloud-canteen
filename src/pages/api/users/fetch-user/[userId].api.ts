b54ff48084a7ac0536e0820c57d5d22856bb8122
ChuongNguyenJH <chuong.nguyen@journeyh.io>
2023-01-18 15:21:02 +0700

    fix duplicate email

---
+++ src/pages/api/users/fetch-user/[userId].api.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import cookies from '@services/cookie';
+import { fetchUser } from '@services/integrationHelper';
+import { handleError } from '@services/sdk';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const { userId } = req.query;
+  try {
+    const user = await fetchUser(userId as string);
+    res.json(user);
+  } catch (error) {
+    handleError(res, error);
+  }
+}
+
+export default cookies(handler);
