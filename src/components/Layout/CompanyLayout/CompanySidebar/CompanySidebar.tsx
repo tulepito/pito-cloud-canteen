5df92971306aa285a2c14966be2f92274f97fad8
ChuongNguyenJH <chuong.nguyen@journeyh.io>
2023-02-06 14:18:12 +0700

    add condition to show group & member setting

--- src/components/Layout/CompanyLayout/CompanySidebar/CompanySidebar.tsx
+++ src/components/Layout/CompanyLayout/CompanySidebar/CompanySidebar.tsx
@@ -2,40 +2,48 @@ import type { TSidebarMenu } from '@components/MultiLevelSidebar/MultiLevelSideb
 import MultiLevelSidebar from '@components/MultiLevelSidebar/MultiLevelSidebar';
 import PitoLogo from '@components/PitoLogo/PitoLogo';
 import { companyPaths } from '@src/paths';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import css from './CompanySidebar.module.scss';
 
-const SIDEBAR_MENUS: TSidebarMenu[] = [
-  {
-    id: 'home',
-    label: 'CompanySidebar.account',
-    nameLink: companyPaths.ContactPoint,
-  },
-  {
-    id: 'user',
-    label: 'CompanySidebar.members',
-    childrenMenus: [
-      {
-        id: 'memberList',
-        label: 'CompanySidebar.memberList',
-        nameLink: '/company/[companyId]/members',
-      },
-      {
-        id: 'groupList',
-        label: 'CompanySidebar.groupList',
-        nameLink: companyPaths.GroupSetting,
-      },
-    ],
-  },
-  {
-    id: 'logo',
-    label: 'CompanySidebar.logo',
-    nameLink: companyPaths.Logo,
-  },
-];
-
 const CompanySidebar = () => {
+  const router = useRouter();
+  const { companyId } = router.query;
+  const SIDEBAR_MENUS: TSidebarMenu[] = [
+    {
+      id: 'home',
+      label: 'CompanySidebar.account',
+      nameLink: companyPaths.ContactPoint,
+    },
+  ];
+  if (companyId) {
+    SIDEBAR_MENUS.push(
+      ...[
+        {
+          id: 'user',
+          label: 'CompanySidebar.members',
+          childrenMenus: [
+            {
+              id: 'memberList',
+              label: 'CompanySidebar.memberList',
+              nameLink: '/company/[companyId]/members',
+            },
+            {
+              id: 'groupList',
+              label: 'CompanySidebar.groupList',
+              nameLink: companyPaths.GroupSetting,
+            },
+          ],
+        },
+        {
+          id: 'logo',
+          label: 'CompanySidebar.logo',
+          nameLink: companyPaths.Logo,
+        },
+      ],
+    );
+  }
   return (
     <div className={css.root}>
       <div className={css.logo}>
