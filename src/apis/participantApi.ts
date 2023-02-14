a249f2164994f5b8d4682a9ab891545584ebad31
nhkhang2002 <khang.nguyen@journeyh.io>
2023-02-14 14:54:44 +0700

    add: api to review restaurant

--- src/apis/participantApi.ts
+++ src/apis/participantApi.ts
@@ -4,6 +4,7 @@ type TParticipantReviewPlanTxBody = {
   txId: string;
   reviewContent: string;
   reviewRating: number;
+  time: string | number;
 };
 export const participantReviewPlanTx = (body: TParticipantReviewPlanTxBody) => {
   return postApi('/participants/plans/review-restaurant', body);
--- src/pages/api/participants/plans/review-restaurant.api.ts
+++ src/pages/api/participants/plans/review-restaurant.api.ts
@@ -2,19 +2,56 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { HttpMethod } from '@apis/configs';
 import cookies from '@services/cookie';
-import { handleError } from '@services/sdk';
+import { getIntegrationSdk } from '@services/integrationSdk';
+import { getSdk, handleError } from '@services/sdk';
+import { CurrentUser, Transaction } from '@utils/data';
+import { merge } from 'lodash';
+import isEmpty from 'lodash/isEmpty';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   try {
     const apiMethod = req.method;
-    // const integrationSdk = getIntegrationSdk();
-    // const sdk = getTrustedSdkWithSubAccountToken;
+    const integrationSdk = getIntegrationSdk();
+    const sdk = getSdk(req, res);
 
     switch (apiMethod) {
       case HttpMethod.GET:
         break;
       case HttpMethod.POST:
+        {
+          const currentUser = await sdk.currentUser.show();
+          const participantId = CurrentUser(currentUser).getId();
+          const { txId, reviewContent, reviewRating, time } = req.body;
+
+          if (isEmpty(txId)) {
+            res.status(400).json({
+              error: 'Transaction ID is empty',
+            });
+          }
+
+          const tx = await integrationSdk.transactions.show({ id: txId });
+          const { reviews = {} } = Transaction(tx).getMetadata();
+          const reviewsAtTime = reviews[time] || {};
+
+          if (isEmpty(reviewsAtTime[participantId])) {
+            res.status(400).json({
+              error: `Participant ${participantId} has already reviewed`,
+            });
+          }
+
+          await integrationSdk.transactions.updateMetadata({
+            reviews: merge(reviews, {
+              [time]: merge(reviewsAtTime, {
+                [participantId]: {
+                  reviewContent,
+                  reviewRating,
+                  authorId: participantId,
+                },
+              }),
+            }),
+          });
+        }
         break;
       case HttpMethod.DELETE:
         break;
