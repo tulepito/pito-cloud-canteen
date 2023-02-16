660d14003d192a7a0364b1508f489817ca7e9930
minhtranjh <minh.tran@journeyh.io>
2023-02-16 13:22:10 +0700

    remove unnecessary code

--- src/pages/api/admin/listings/order/query.api.ts
+++ src/pages/api/admin/listings/order/query.api.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import cookies from '@services/cookie';
-import { deserialize, getIntegrationSdk, handleError } from '@services/sdk';
+import { getIntegrationSdk, handleError } from '@services/sdk';
 import { LISTING_TYPE } from '@src/pages/api/helpers/constants';
 import { denormalisedResponseEntities } from '@utils/data';
 import { EListingType } from '@utils/enums';
@@ -10,19 +10,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   try {
-    if (
-      req.headers['content-type'] === 'application/transit+json' &&
-      typeof req.body === 'string'
-    ) {
-      try {
-        req.body = deserialize(req.body);
-      } catch (e) {
-        console.error('Failed to parse request body as Transit:');
-        console.error(e);
-        res.status(400).send('Invalid Transit in request body.');
-        return;
-      }
-    }
     const { dataParams = {}, queryParams = {} } = req.body;
     const integrationSdk = getIntegrationSdk();
     const response = await integrationSdk.listings.query(
