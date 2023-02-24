70d5d09a0cd426e4931e6cea837be9b34e96d13e
nhkhang2002 <khang.nguyen@journeyh.io>
2023-02-24 15:18:26 +0700

    add: job and run jobs for scheduleJobForUpdatingOrderState

--- src/serverless/scheduleJobForUpdatingOrderState/index.js
+++ src/serverless/scheduleJobForUpdatingOrderState/index.js
@@ -1,20 +1,53 @@
-// exports.handler = (event, context) => {
-
-const { queryEventsByEventType } = require('./utils/dataPreparation');
+/* eslint-disable no-unused-vars */
+const get = require('lodash/get');
+const isEmpty = require('lodash/isEmpty');
+const mapLimit = require('async/mapLimit');
+const {
+  queryEventsByEventType,
+  setLatestEventSequenceId,
+} = require('./utils/dataPreparation');
+const { findValidEvents } = require('./utils/findValidEvents');
 const { EventTypes } = require('./utils/constants');
+const { denormalisedResponseEntities, FlexEvent } = require('./utils/data');
+const { completeOrder } = require('./jobs');
+const { findLastSequenceId } = require('./utils/findLastSequenceId');
 
-const handler = async () => {
+exports.handler = async (_event, _context) => {
+  // const handler = async () => {
   try {
     console.log(
       'Start to run schedule to scan transactions and update order data ...',
     );
 
     const eventTypes = EventTypes.transitTransaction;
-    const eventList = await queryEventsByEventType(eventTypes);
-    console.info(
-      '🚀 ~ handler ~ eventList',
-      JSON.stringify(eventList, null, 2),
+    const eventList = denormalisedResponseEntities(
+      await queryEventsByEventType(eventTypes),
     );
+    const needHandleEvents = findValidEvents(eventList);
+    const orderList = needHandleEvents.map((event) => {
+      const normalizedEvent = FlexEvent(event);
+      const { data } = normalizedEvent.getResource();
+      const { orderId } = get(data, 'attributes.metadata', {});
+
+      return orderId;
+    });
+
+    if (isEmpty(orderList)) {
+      console.info('No order need to update state ...');
+      return;
+    }
+    //
+    console.info('Start to update order state ...');
+    await mapLimit(orderList, 10, completeOrder);
+    //
+    console.info('Find last sequence id ...');
+    const lastSequenceId = findLastSequenceId(eventList);
+    console.info('Found id ', lastSequenceId);
+    //
+    if (lastSequenceId) {
+      console.info('Update latest sequence id ...', lastSequenceId);
+      setLatestEventSequenceId(lastSequenceId);
+    }
   } catch (error) {
     console.error(
       'Schedule scan transaction and update order data',
@@ -23,4 +56,4 @@ const handler = async () => {
   }
 };
 
-handler();
+// handler();
---
+++ src/serverless/scheduleJobForUpdatingOrderState/jobs.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-useless-return */
+const { OrderStates } = require('./utils/constants');
+const { denormalisedResponseEntities } = require('./utils/data');
+const { integrationSdk } = require('./utils/sdk');
+
+const completeOrder = async (orderId) => {
+  console.info('Start to complete order: ', orderId);
+
+  const [orderListing] = denormalisedResponseEntities(
+    await integrationSdk.listings.show({
+      id: orderId,
+    }),
+  );
+
+  const { orderState, orderStateHistory = [] } =
+    orderListing?.attributes?.metadata || {};
+
+  if (orderState !== OrderStates.inProgress) {
+    console.error('Order is not in progress, skip complete order');
+    return;
+  }
+
+  await integrationSdk.listings.update({
+    id: orderId,
+    metadata: {
+      orderState: OrderStates.pendingPayment,
+      orderStateHistory: orderStateHistory.concat({
+        state: OrderStates.pendingPayment,
+        updatedAt: new Date().getTime(),
+      }),
+    },
+  });
+
+  console.info('Complete order successfully, orderId: ', orderId);
+};
+
+module.exports = { completeOrder };
