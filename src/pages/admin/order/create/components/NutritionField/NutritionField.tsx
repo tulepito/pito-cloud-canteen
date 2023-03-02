17e87ff3250db68f760359340d1f148ba6b71422
ChuongNguyenJH <chuong.nguyen@journeyh.io>
2023-03-03 00:20:51 +0700

    fix UI bug

--- src/pages/admin/order/create/components/NutritionField/NutritionField.tsx
+++ src/pages/admin/order/create/components/NutritionField/NutritionField.tsx
@@ -1,24 +1,27 @@
 import FieldCheckbox from '@components/FormFields/FieldCheckbox/FieldCheckbox';
 import { nutritionOptions } from '@src/marketplaceConfig';
+import classNames from 'classnames';
 import { useIntl } from 'react-intl';
 
 import css from './NutritionField.module.scss';
 
 type NutritionFieldProps = {
   title?: string;
+  titleClassName?: string;
+  fieldClassName?: string;
 };
 
 const NutritionField: React.FC<NutritionFieldProps> = (props) => {
-  const { title } = props;
+  const { title, titleClassName, fieldClassName } = props;
   const intl = useIntl();
   return (
     <div className={css.container}>
       {title && (
-        <div className={css.fieldTitle}>
+        <div className={classNames(css.fieldTitle, titleClassName)}>
           {intl.formatMessage({ id: 'NutritionField.title' })}
         </div>
       )}
-      <div className={css.fieldGroups}>
+      <div className={classNames(css.fieldGroups, fieldClassName)}>
         {nutritionOptions.map(({ key, label }) => (
           <FieldCheckbox
             key={key}
