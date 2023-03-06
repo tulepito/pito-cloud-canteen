c5ad53e657696d6c3db66f80adcc5dd226ef6d31
minhtranjh <minh.tran@journeyh.io>
2023-03-06 11:26:26 +0700

    remove unused code

--- src/pages/admin/partner/[restaurantId]/settings/food/utils.ts
+++ src/pages/admin/partner/[restaurantId]/settings/food/utils.ts
@@ -1,8 +1,7 @@
 import { types as sdkTypes } from '@helpers/sdkLoader';
 import { ListingTypes } from '@src/types/listingTypes';
-import { EListingStates, EMenuTypes } from '@utils/enums';
+import type { EMenuTypes } from '@utils/enums';
 import { getSubmitImageId, getUniqueImages } from '@utils/images';
-import { removeAccents } from '@utils/string';
 import type { TImage } from '@utils/types';
 
 const { Money } = sdkTypes;
@@ -117,85 +116,32 @@ export const getDuplicateData = (values: TEditPartnerFoodFormValues) => {
   };
 };
 
-const CSV_TABLE_HEADER_IN_VI = {
-  'Tên món ăn': 'title',
-  'Loại menu': 'menuType',
-  'Phân loại': 'foodType',
-  'Mô tả chi tiết': 'description',
-  'Chất liệu bao bì': 'packagingMaterial',
-  'Đơn giá (vnđ)': 'price',
-  'Số món chính (món)': 'numberOfMainDishes',
-  'Món xào': 'sideDishes.stir-fried-meal',
-  'Món canh': 'sideDishes.soup',
-  'Tráng miệng': 'sideDishes.dessert',
-  'Nước uống': 'sideDishes.drink',
-  'Thành phần dị ứng': 'allergicIngredients',
-};
-
-const MENU_TYPES_FROM_CSV = {
-  'menu co dinh': EMenuTypes.fixedMenu,
-  'menu theo chu ky': EMenuTypes.cycleMenu,
-};
-
 export const getImportDataFromCsv = (values: any) => {
-  const newValues = Object.keys(values).reduce((prev: any, key: any) => {
-    const parsedKey = removeAccents(String(key).toLowerCase().trim());
-    const keyAsVn = Object.keys(CSV_TABLE_HEADER_IN_VI).find((k) => {
-      const keyToCompare = removeAccents(String(k).toLowerCase().trim());
-      return keyToCompare === parsedKey;
-    });
-    const newKey =
-      CSV_TABLE_HEADER_IN_VI[keyAsVn as keyof typeof CSV_TABLE_HEADER_IN_VI];
-
-    return {
-      ...prev,
-      ...(newKey ? { [newKey]: values[key] } : { [key]: values[key] }),
-    };
-  }, {});
-
   const {
+    images = [],
     title,
     description,
     price,
+    // eslint-disable-next-line unused-imports/no-unused-vars
+    addImages,
     restaurantId,
-    allergicIngredients,
-    numberOfMainDishes,
-    packagingMaterial,
-    foodType,
-    menuType,
+    specialDiets,
+    sideDishes,
     ...rest
-  } = newValues;
-
-  const sideDishes = Object.keys(rest).reduce((prev: any, key: any) => {
-    const [valueKey] = key.split('.');
-    return [...prev, valueKey];
-  }, []);
-
-  const menuTypeWithoutAccent = removeAccents(
-    String(menuType).toLowerCase().trim(),
-  );
-
-  const formattedMenuType =
-    MENU_TYPES_FROM_CSV[
-      menuTypeWithoutAccent as keyof typeof MENU_TYPES_FROM_CSV
-    ];
-
+  } = values;
   return {
+    ...(images ? { images: images.filter((i: TImage) => !!i) } : {}),
     title,
     description,
     price: parsePriceToMoneyFormat(price),
     publicData: {
-      sideDishes,
-      allergicIngredients,
-      numberOfMainDishes: Number(numberOfMainDishes),
-      packagingMaterial,
-      foodType,
+      specialDiets: specialDiets.split(','),
+      sideDishes: sideDishes.split(','),
+      ...rest,
     },
     metadata: {
-      menuType: formattedMenuType,
       restaurantId,
       listingType: ListingTypes.FOOD,
-      listingStates: EListingStates.published,
     },
   };
 };
