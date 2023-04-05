550984571da47aa3bd2f92ac2feed9648d9daffa
minhtranjh <minh.tran@journeyh.io>
2023-04-05 18:20:50 +0700

    update check condition for duplicate menu api

--- src/pages/api/admin/listings/menus/[menuId]/duplicate.api.ts
+++ src/pages/api/admin/listings/menus/[menuId]/duplicate.api.ts
@@ -1,8 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { HttpMethod } from '@apis/configs';
+import checkUnConflictedMenuMiddleware from '@pages/api/apiServices/menu/checkUnConflictedMenuMiddleware.service';
 import duplicateMenu from '@pages/api/apiServices/menu/duplicateMenu.service';
+import cookies from '@services/cookie';
+import { denormalisedResponseEntities } from '@services/data';
+import { getIntegrationSdk } from '@services/integrationSdk';
+import adminChecker from '@services/permissionChecker/admin';
 import { handleError } from '@services/sdk';
+import { IntegrationListing } from '@src/utils/data';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   try {
@@ -29,4 +35,59 @@ async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   }
 }
 
-export default handler;
+const handlerWrapper = async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case HttpMethod.POST: {
+      const { dataParams = {} } = req.body;
+      const { menuId } = req.query;
+
+      const {
+        mealType,
+        daysOfWeek = [],
+        startDate,
+        endDate,
+        restaurantId,
+      } = dataParams;
+
+      const integrationSdk = getIntegrationSdk();
+
+      const menuResponse = await integrationSdk.listings.show(
+        {
+          id: menuId,
+        },
+        {
+          expand: true,
+        },
+      );
+
+      const [menu] = denormalisedResponseEntities(menuResponse);
+
+      const {
+        mealType: mealTyperFromMenu,
+        startDate: startDateFromMenu,
+        endDate: endDateFromMenu,
+        daysOfWeek: daysOfWeekFromMenu,
+      } = IntegrationListing(menu).getPublicData();
+
+      const { restaurantId: restaurantIdFromMenu } =
+        IntegrationListing(menu).getMetadata();
+
+      const dataToCheck = {
+        ...(mealType ? { mealType } : { mealType: mealTyperFromMenu }),
+        ...(restaurantId
+          ? { restaurantId }
+          : { restaurantId: restaurantIdFromMenu }),
+        ...(daysOfWeek ? { daysOfWeek } : { daysOfWeek: daysOfWeekFromMenu }),
+        ...(startDate ? { startDate } : { startDate: startDateFromMenu }),
+        ...(endDate ? { endDate } : { endDate: endDateFromMenu }),
+      };
+
+      return checkUnConflictedMenuMiddleware(handler)(req, res, dataToCheck);
+    }
+
+    default:
+      return handler(req, res);
+  }
+};
+
+export default cookies(adminChecker(handlerWrapper));
