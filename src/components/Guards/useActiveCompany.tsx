b2ab0921914fc32686ae79be2633536f68c5ce9b
minhtranjh <minh.tran@journeyh.io>
2023-04-21 16:22:50 +0700

    fix check inactive company

--- src/components/Guards/useActiveCompany.tsx
+++ src/components/Guards/useActiveCompany.tsx
@@ -1,14 +1,15 @@
 import { useAppSelector } from '@hooks/reduxHooks';
 import { User } from '@utils/data';
-import { ECompanyStatus, EUserPermission } from '@utils/enums';
+import { ECompanyStates, EUserPermission } from '@utils/enums';
 import type { TUser } from '@utils/types';
 
 const useActiveCompany = () => {
   const { userPermission, currentUser } = useAppSelector((state) => state.user);
-  const { status } = User(currentUser as unknown as TUser).getMetadata();
+  const { userState } = User(currentUser as unknown as TUser).getMetadata();
+
   const isInactiveCompany =
     userPermission === EUserPermission.company &&
-    status === ECompanyStatus.unactive;
+    userState === ECompanyStates.unactive;
 
   return { isInactiveCompany };
 };
--- src/components/InActiveUserScreen/InActiveUserScreen.tsx
+++ src/components/InActiveUserScreen/InActiveUserScreen.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 import Button from '@components/Button/Button';
+import PitoLogo from '@components/PitoLogo/PitoLogo';
 import { useAppDispatch } from '@hooks/reduxHooks';
 import { authThunks } from '@redux/slices/auth.slice';
 import { userActions } from '@redux/slices/user.slice';
 import { generalPaths } from '@src/paths';
+
+import css from './InActiveUserScreen.module.scss';
 
 const UnActiveUserScreen = () => {
   const dispatch = useAppDispatch();
@@ -18,8 +21,9 @@ const UnActiveUserScreen = () => {
   };
 
   return (
-    <div>
-      <p>404 Error</p>
+    <div className={css.root}>
+      <PitoLogo />
+      <p>Tài khoản của bạn chưa được kích hoạt</p>
       <Button type="button" onClick={handleLogout}>
         Đăng xuất
       </Button>
