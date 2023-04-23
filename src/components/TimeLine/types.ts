c4ebd508def2e73687ef21ca3d4d60ddb21771ae
Allen <khang.nguyen@journeyh.io>
2023-04-23 19:48:28 +0700

    update: time line component
    - add: state item tooltip
    - add: admin layout condition, show tooltip for state item with admin layout

--- src/components/TimeLine/types.ts
+++ src/components/TimeLine/types.ts
@@ -1,10 +1,14 @@
 import type { TDefaultProps, TObject } from '@utils/types';
 
 export type TTimeLineItemData = TObject;
-export type TTimeLineItemProps = TDefaultProps & { data: TTimeLineItemData };
+export type TTimeLineItemProps = TDefaultProps & {
+  data: TTimeLineItemData;
+  isAdminLayout?: boolean;
+};
 
 export type TTimeLineProps = TDefaultProps & {
   items: TTimeLineItemData[];
   itemComponent: React.FC<TTimeLineItemProps>;
   itemClassName?: string;
+  isAdminLayout?: boolean;
 };
