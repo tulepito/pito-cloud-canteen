684df8763c500ddb3c2ec460e69530bdecabcedd
Allen <khang.nguyen@journeyh.io>
2023-05-18 09:22:19 +0700

    update: user name in AvatarForm

--- src/pages/participant/account/components/AvatarForm/AvatarForm.tsx
+++ src/pages/participant/account/components/AvatarForm/AvatarForm.tsx
@@ -200,8 +200,8 @@ const AvatarFormComponent: React.FC<TAvatarFormComponentProps> = (props) => {
         </Field>
       </div>
       <div className={css.userName}>
-        {`${currentUserGetter.getProfile().lastName} ${
-          currentUserGetter.getProfile().firstName
+        {`${currentUserGetter.getProfile().firstName} ${
+          currentUserGetter.getProfile().lastName
         }`}
       </div>
     </Form>
