60794b1e7b8b37f40483ad1d13a9cc884662f7a4
Allen <khang.nguyen@journeyh.io>
2023-05-30 11:32:44 +0700

    update: clear change password error when access change password page

--- src/pages/participant/account/change-password/index.route.tsx
+++ src/pages/participant/account/change-password/index.route.tsx
@@ -1,16 +1,27 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 import MetaWrapper from '@components/MetaWrapper/MetaWrapper';
+import { useAppDispatch } from '@hooks/reduxHooks';
 import { participantPaths } from '@src/paths';
 
+import { AccountActions } from '../Account.slice';
 import ChangePasswordModal from '../components/ChangePasswordModal/ChangePasswordModal';
 
-const AccountPageRoute = () => {
+const ChangePasswordRoute = () => {
   const router = useRouter();
+  const dispatch = useAppDispatch();
 
   const goBack = () => {
     router.push(participantPaths.Account);
   };
+
+  useEffect(() => {
+    return () => {
+      dispatch(AccountActions.clearChangePasswordError());
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <MetaWrapper>
@@ -19,4 +30,4 @@ const AccountPageRoute = () => {
   );
 };
 
-export default AccountPageRoute;
+export default ChangePasswordRoute;
