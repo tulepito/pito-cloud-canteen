0fd4fe3ba793e118f25b0e542a2ef4d0690b878c
Allen <khang.nguyen@journeyh.io>
2023-06-16 16:15:37 +0700

    PCC-78 update: order flow in order helper

--- src/helpers/orderHelper.ts
+++ src/helpers/orderHelper.ts
@@ -392,7 +392,7 @@ export const orderFlow = {
     EOrderStates.canceledByBooker,
     EOrderStates.canceled,
   ],
-  [EOrderStates.picking]: [EOrderStates.inProgress],
+  [EOrderStates.picking]: [EOrderStates.inProgress, EOrderStates.canceled],
   [EOrderStates.inProgress]: [EOrderStates.pendingPayment],
   [EOrderStates.pendingPayment]: [EOrderStates.completed],
   [EOrderStates.completed]: [EOrderStates.reviewed],
