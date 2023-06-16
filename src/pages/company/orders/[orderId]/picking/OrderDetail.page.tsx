d624e7cee8008f3c9e237bf9c7f4158276f1b5a6
minhtranjh <minh.tran@journeyh.io>
2023-06-16 14:21:50 +0700

    handle merge draft order detail to main order detail

--- src/pages/company/orders/[orderId]/picking/OrderDetail.page.tsx
+++ src/pages/company/orders/[orderId]/picking/OrderDetail.page.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useCallback, useEffect, useState } from 'react';
 import { useIntl } from 'react-intl';
-import Skeleton from 'react-loading-skeleton';
 import classNames from 'classnames';
 import isEmpty from 'lodash/isEmpty';
 import { useRouter } from 'next/router';
@@ -482,8 +481,39 @@ const OrderDetailPage = () => {
           </RenderWhen.False>
         </RenderWhen>
         <RenderWhen.False>
-          <div className={css.loadingContainer}>
-            <Skeleton className={css.loadingContent} />
+          <div className={css.lineItemsTable}>
+            <ManageLineItemsSection
+              data={editViewData.manageOrdersData}
+              isDraftEditing={isDraftEditing}
+              shouldShowOverflowError={shouldShowOverflowError}
+              shouldShowUnderError={shouldShowUnderError}
+              setCurrentViewDate={(date) => setCurrentViewDate(date)}
+              currentViewDate={currentViewDate}
+            />
+            {isDraftEditing && (
+              <SubOrderChangesHistorySection
+                className={classNames(
+                  css.container,
+                  css.normalOrderSubOrderSection,
+                )}
+                querySubOrderChangesHistoryInProgress={
+                  querySubOrderChangesHistoryInProgress
+                }
+                subOrderChangesHistory={subOrderChangesHistory}
+                draftSubOrderChangesHistory={
+                  draftSubOrderChangesHistory[
+                    currentViewDate as unknown as keyof typeof draftSubOrderChangesHistory
+                  ]
+                }
+                onQueryMoreSubOrderChangesHistory={
+                  onQueryMoreSubOrderChangesHistory
+                }
+                subOrderChangesHistoryTotalItems={
+                  subOrderChangesHistoryTotalItems
+                }
+                loadMoreSubOrderChangesHistory={loadMoreSubOrderChangesHistory}
+              />
+            )}
           </div>
         </RenderWhen.False>
       </RenderWhen>
