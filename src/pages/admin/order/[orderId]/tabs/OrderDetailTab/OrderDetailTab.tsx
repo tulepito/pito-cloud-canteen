ff4a15dbbd8fc4d94369de540c9840640fdce000
Allen <khang.nguyen@journeyh.io>
2023-06-16 15:52:22 +0700

    PCC-78 update: logic and UI picking, review and start order sections.

--- src/pages/admin/order/[orderId]/tabs/OrderDetailTab/OrderDetailTab.tsx
+++ src/pages/admin/order/[orderId]/tabs/OrderDetailTab/OrderDetailTab.tsx
@@ -1,12 +1,20 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import isEmpty from 'lodash/isEmpty';
 
+import ManageOrdersSection from '@components/OrderDetails/EditView/ManageOrderDetailSection/ManageOrdersSection';
+import ManageParticipantsSection from '@components/OrderDetails/EditView/ManageParticipantsSection/ManageParticipantsSection';
+import OrderDeadlineCountdownSection from '@components/OrderDetails/EditView/OrderDeadlineCountdownSection/OrderDeadlineCountdownSection';
+import OrderLinkSection from '@components/OrderDetails/EditView/OrderLinkSection/OrderLinkSection';
 import ReviewOrderStatesSection from '@components/OrderDetails/ReviewView/ReviewOrderStatesSection/ReviewOrderStatesSection';
+import ReviewView from '@components/OrderDetails/ReviewView/ReviewView';
 import RenderWhen from '@components/RenderWhen/RenderWhen';
 import Tabs from '@components/Tabs/Tabs';
+import { groupFoodOrderByDate } from '@helpers/order/orderDetailHelper';
 import { useAppDispatch } from '@hooks/reduxHooks';
+import { useDownloadPriceQuotation } from '@hooks/useDownloadPriceQuotation';
+import { usePrepareOrderDetailPageData } from '@hooks/usePrepareOrderManagementData';
 import { AdminAttributesThunks } from '@pages/admin/Attributes.slice';
 import { ReviewContent } from '@pages/admin/order/create/components/ReviewOrder/ReviewOrder';
-import { groupFoodOrderByDate } from '@pages/company/orders/[orderId]/picking/helpers/orderDetailHelper';
 import { Listing } from '@src/utils/data';
 import { formatTimestamp } from '@src/utils/dates';
 import { EOrderStates } from '@src/utils/enums';
@@ -17,6 +25,12 @@ import OrderHeaderState from '../../components/OrderHeaderState/OrderHeaderState
 import { OrderDetailThunks } from '../../OrderDetail.slice';
 
 import css from './OrderDetailTab.module.scss';
+
+enum EPageViewMode {
+  edit = 'edit',
+  review = 'review',
+  priceQuotation = 'priceQuotation',
+}
 
 type OrderDetailTabProps = {
   order: TListing;
@@ -44,18 +58,33 @@ const OrderDetailTab: React.FC<OrderDetailTabProps> = (props) => {
     updateOrderStateInProgress,
     transactionDataMap,
   } = props;
+
+  const [viewMode, setViewMode] = useState<EPageViewMode>(EPageViewMode.edit);
+
   const dispatch = useAppDispatch();
   const orderId = Listing(order).getId();
   const {
     notes,
     orderStateHistory = [],
     plans = [],
+    orderState,
+    orderType = 'group',
   } = Listing(order).getMetadata();
+  const isPickingState = orderState === EOrderStates.picking;
   const planId = plans.length > 0 ? plans[0] : undefined;
   const showStateSectionCondition =
     orderStateHistory.findIndex(({ state }: { state: EOrderStates }) => {
       return state === EOrderStates.inProgress;
     }) > 0;
+
+  const { orderTitle, priceQuotationData, editViewData, reviewViewData } =
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    usePrepareOrderDetailPageData();
+
+  const handleDownloadPriceQuotation = useDownloadPriceQuotation(
+    orderTitle,
+    priceQuotationData,
+  );
 
   const tabItems = useMemo(
     () => {
@@ -100,44 +129,100 @@ const OrderDetailTab: React.FC<OrderDetailTabProps> = (props) => {
     [JSON.stringify(order), JSON.stringify(orderDetail)],
   );
 
-  const handleUpdateOrderState = () => {
-    updateOrderState(EOrderStates.picking);
+  const handleConfirmOrder = () => {
+    setViewMode(EPageViewMode.review);
   };
 
-  const handleCancelOrder = () => {
-    updateOrderState(EOrderStates.canceled);
+  const handleGoBackFromReviewMode = () => {
+    setViewMode(EPageViewMode.edit);
+  };
+
+  const handleUpdateOrderState = (state: EOrderStates) => () => {
+    updateOrderState(state);
   };
 
   useEffect(() => {
     dispatch(AdminAttributesThunks.fetchAttributes());
   }, []);
 
+  useEffect(() => {
+    if (!isEmpty(orderState)) {
+      setViewMode(isPickingState ? EPageViewMode.edit : EPageViewMode.review);
+    }
+  }, [orderState]);
+
   return (
     <div className={css.container}>
-      <OrderHeaderState
-        order={order}
-        handleUpdateOrderState={handleUpdateOrderState}
-        updateOrderStateInProgress={updateOrderStateInProgress}
-        handleCancelOrder={handleCancelOrder}
-      />
-      <RenderWhen condition={showStateSectionCondition}>
-        <ReviewOrderStatesSection
-          data={{ transactionDataMap, isCanceledOrder: false }}
-          isAdminLayout
-          className={css.reviewOrderStates}
+      <RenderWhen condition={viewMode === EPageViewMode.edit}>
+        <OrderHeaderState
+          order={order}
+          handleUpdateOrderState={handleUpdateOrderState}
+          updateOrderStateInProgress={updateOrderStateInProgress}
+          onConfirmOrder={handleConfirmOrder}
         />
+        <RenderWhen condition={showStateSectionCondition}>
+          <ReviewOrderStatesSection
+            data={{ transactionDataMap, isCanceledOrder: false }}
+            isAdminLayout
+            className={css.reviewOrderStates}
+          />
+        </RenderWhen>
+        <OrderHeaderInfor
+          company={company}
+          booker={booker}
+          order={order}
+          updateStaffName={updateStaffName}
+          updateOrderStaffNameInProgress={updateOrderStaffNameInProgress}
+          containerClassName={css.orderInforWrapper}
+        />
+
+        <div className={css.orderDetailWrapper}>
+          <RenderWhen condition={isPickingState}>
+            <RenderWhen condition={orderType === 'group'}>
+              <div className={css.editViewRoot}>
+                <div className={css.leftPart}>
+                  <ManageOrdersSection data={editViewData.manageOrdersData} />
+                </div>
+                <div className={css.rightPart}>
+                  <OrderDeadlineCountdownSection
+                    className={css.container}
+                    data={editViewData.countdownSectionData}
+                  />
+                  <OrderLinkSection
+                    className={css.container}
+                    data={editViewData.linkSectionData}
+                    isAminLayout
+                  />
+                  <ManageParticipantsSection
+                    className={css.container}
+                    data={editViewData.manageParticipantData}
+                  />
+                </div>
+              </div>
+            </RenderWhen>
+
+            <RenderWhen.False>
+              <Tabs items={tabItems as any} />
+            </RenderWhen.False>
+          </RenderWhen>
+        </div>
+
+        <RenderWhen.False>
+          <ReviewView
+            className={css.reviewViewRoot}
+            classes={{
+              leftClassName: css.leftPart,
+              rightClassName: css.rightPart,
+            }}
+            canGoBackEditMode
+            reviewViewData={reviewViewData}
+            onSubmitEdit={() => {}}
+            onDownloadPriceQuotation={handleDownloadPriceQuotation}
+            onGoBackToEditOrderPage={handleGoBackFromReviewMode}
+            showStartPickingOrderButton
+          />
+        </RenderWhen.False>
       </RenderWhen>
-      <OrderHeaderInfor
-        company={company}
-        booker={booker}
-        order={order}
-        updateStaffName={updateStaffName}
-        updateOrderStaffNameInProgress={updateOrderStaffNameInProgress}
-        containerClassName={css.orderInforWrapper}
-      />
-      <div className={css.orderDetailWrapper}>
-        <Tabs items={tabItems as any} />
-      </div>
     </div>
   );
 };
