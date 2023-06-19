0b16e172fd0347e5401c50b024799f391a0c907b
ChuongNguyenJH <chuong.nguyen@journeyh.io>
2023-06-19 14:27:07 +0700

    revert start order thunk

--- src/redux/slices/OrderManagement.slice.ts
+++ src/redux/slices/OrderManagement.slice.ts
@@ -1518,9 +1518,8 @@ const OrderManagementSlice = createSlice({
       .addCase(bookerStartOrder.pending, (state) => {
         state.isStartOrderInProgress = true;
       })
-      .addCase(bookerStartOrder.fulfilled, (state, { payload }) => {
+      .addCase(bookerStartOrder.fulfilled, (state) => {
         state.isStartOrderInProgress = false;
-        state.quotation = payload;
       })
       .addCase(bookerStartOrder.rejected, (state) => {
         state.isStartOrderInProgress = false;
