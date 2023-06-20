080e4a95b71fc2b3c7d34e3bff91006e7b184025
minhtranjh <minh.tran@journeyh.io>
2023-06-20 10:42:52 +0700

    fix disabled submit button

--- src/pages/company/orders/[orderId]/picking/OrderDetail.page.tsx
+++ src/pages/company/orders/[orderId]/picking/OrderDetail.page.tsx
@@ -214,7 +214,9 @@ const OrderDetailPage = () => {
         id: 'EditView.OrderTitle.updateOrderButtonText',
       });
 
-  const ableToUpdateOrder = diffDays(currentViewDate, NOW, 'day') > ONE_DAY;
+  const ableToUpdateOrder =
+    (isDraftEditing && diffDays(currentViewDate, NOW, 'day') > ONE_DAY) ||
+    isPicking;
 
   const orderDetailsNotChanged =
     isDraftEditing && isEqual(orderDetail, newOrderDetail);
@@ -240,9 +242,10 @@ const OrderDetailPage = () => {
             : ''
         }
         confirmDisabled={
-          orderDetailsNotChanged ||
-          (!isDraftEditing && shouldShowOverflowError) ||
-          (!isDraftEditing && shouldShowUnderError)
+          !isPicking &&
+          (orderDetailsNotChanged ||
+            (!isDraftEditing && shouldShowOverflowError) ||
+            (!isDraftEditing && shouldShowUnderError))
         }
         isDraftEditing={isDraftEditing}
       />
