84a0ec759233dff6958d95f7c056308b31d84c2a
minhtranjh <minh.tran@journeyh.io>
2023-06-27 17:20:52 +0700

    fix update order detail error message

--- src/redux/slices/OrderManagement.slice.ts
+++ src/redux/slices/OrderManagement.slice.ts
@@ -123,20 +123,14 @@ export const checkMinMaxQuantity = (
     const data = orderDetails[key] || {};
     const { memberOrders = {}, restaurant } = data;
     const { minQuantity = 1 } = restaurant;
-    const { memberOrders: oldMemberOrders = {} } = oldOrderDetail?.[key] || {};
-    const oldTotalQuantity = Object.keys(oldMemberOrders).filter(
-      (f) =>
-        !!oldMemberOrders[f].foodId &&
-        oldMemberOrders[f].status === EParticipantOrderStatus.joined,
-    ).length;
+
     const totalQuantity = Object.keys(memberOrders).filter(
       (f) =>
         !!memberOrders[f].foodId &&
         memberOrders[f].status === EParticipantOrderStatus.joined,
     ).length;
-    const totalQuantityCanAdd = (totalQuantity * 10) / 100;
-    const totalAdded = totalQuantity - oldTotalQuantity;
-    const shouldShowOverflowError = totalAdded > totalQuantityCanAdd;
+
+    const shouldShowOverflowError = totalQuantity > maxQuantity;
 
     const shouldShowUnderError = totalQuantity < minQuantity;
 
