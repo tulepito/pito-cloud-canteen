10f57c7ad9cad67a57f24996c3a23b5217ecfc43
minhtranjh <minh.tran@journeyh.io>
2023-06-27 11:42:22 +0700

    prevent update order detail when reach min max

--- src/redux/slices/OrderManagement.slice.ts
+++ src/redux/slices/OrderManagement.slice.ts
@@ -141,7 +141,6 @@ export const checkMinMaxQuantityInProgressState = (
     ).length;
     const totalQuantityCanAdd = (totalQuantity * 10) / 100;
     const totalAdded = totalQuantity - oldTotalQuantity;
-
     const shouldShowOverflowError = totalAdded > totalQuantityCanAdd;
 
     const shouldShowUnderError = totalQuantity < minQuantity;
