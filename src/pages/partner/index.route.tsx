62d9b586cb3e3f7872513ffeceecbce37b4b151c
Allen <khang.nguyen@journeyh.io>
2023-06-29 14:53:22 +0700

    PCC-46: init partner pages

---
+++ src/pages/partner/index.route.tsx
@@ -0,0 +1,9 @@
+import MetaWrapper from '@components/MetaWrapper/MetaWrapper';
+
+export default function PartnerHomePageRoute() {
+  return (
+    <MetaWrapper routeName="PartnerHomePageRoute">
+      <div></div>
+    </MetaWrapper>
+  );
+}
---
+++ src/pages/partner/orders/index.route.tsx
@@ -0,0 +1,9 @@
+import MetaWrapper from '@components/MetaWrapper/MetaWrapper';
+
+export default function PartnerOrderPageRoute() {
+  return (
+    <MetaWrapper routeName="PartnerOrderPageRoute">
+      <div></div>
+    </MetaWrapper>
+  );
+}
