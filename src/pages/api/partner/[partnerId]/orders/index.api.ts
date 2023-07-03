f764be50114974850968221989b7f554c7b10d6a
Allen <khang.nguyen@journeyh.io>
2023-07-04 02:26:40 +0700

    PCC-46 add; logic query tx in query partner orders api

--- src/pages/api/partner/[partnerId]/orders/index.api.ts
+++ src/pages/api/partner/[partnerId]/orders/index.api.ts
@@ -1,4 +1,4 @@
-import { mapLimit } from 'async';
+import { asyncify, mapLimit } from 'async';
 import isEmpty from 'lodash/isEmpty';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
@@ -7,8 +7,9 @@ import { queryAllListings } from '@helpers/apiHelpers';
 import { denormalisedResponseEntities } from '@services/data';
 import { getIntegrationSdk, handleError } from '@services/sdk';
 import { Listing } from '@src/utils/data';
-import { EListingType } from '@src/utils/enums';
-import type { TListing } from '@src/utils/types';
+import { EListingType, EOrderStates } from '@src/utils/enums';
+import type { TPlan } from '@src/utils/orderTypes';
+import type { TListing, TObject } from '@src/utils/types';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   try {
@@ -28,9 +29,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
         });
 
         const orderWithPlanDataMaybe = async (order: TListing) => {
-          const { plans = [] } = Listing(order).getMetadata();
+          const { plans = [], orderState } = Listing(order).getMetadata();
 
           const planId = plans[0];
+          let resultValue = order as TObject;
 
           if (planId) {
             const [planListing] = denormalisedResponseEntities(
@@ -38,7 +40,44 @@ async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
             );
 
             if (!isEmpty(planListing)) {
-              return { ...order, plan: planListing };
+              resultValue = { ...resultValue, plan: planListing };
+              const { orderDetail } = Listing(planListing).getMetadata();
+
+              if (
+                [
+                  EOrderStates.inProgress,
+                  EOrderStates.pendingPayment,
+                  EOrderStates.completed,
+                  EOrderStates.reviewed,
+                ].includes(orderState)
+              ) {
+                const transactionIdMap = Object.entries<
+                  TPlan['orderDetail'][keyof TPlan['orderDetail']]
+                >(orderDetail).reduce<
+                  { timestamp: string; transactionId: string }[]
+                >((prev, [timestamp, { transactionId }]) => {
+                  if (transactionId)
+                    return prev.concat([{ timestamp, transactionId }]);
+
+                  return prev;
+                }, []);
+
+                const transactionDataMap: TObject = {};
+                await Promise.all(
+                  transactionIdMap.map(async ({ timestamp, transactionId }) => {
+                    const [tx] = denormalisedResponseEntities(
+                      await integrationSdk.transactions.show({
+                        id: transactionId,
+                      }),
+                    );
+
+                    transactionDataMap[timestamp] = tx;
+                  }),
+                );
+                resultValue = { ...resultValue, transactionDataMap };
+              }
+
+              return resultValue;
             }
           }
 
@@ -48,7 +87,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
         const ordersWithPlanData = await mapLimit(
           orders,
           10,
-          orderWithPlanDataMaybe,
+          asyncify(orderWithPlanDataMaybe),
         );
 
         return res.status(200).json({ orders: ordersWithPlanData });
