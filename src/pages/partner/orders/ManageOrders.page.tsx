c7d74961157e55825495818a776c66e5e002e0b1
Allen <khang.nguyen@journeyh.io>
2023-07-04 02:27:34 +0700

    PCC-46 add: sort, filter logic for ManageOrdersPage

--- src/pages/partner/orders/ManageOrders.page.tsx
+++ src/pages/partner/orders/ManageOrders.page.tsx
@@ -1,9 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/no-shadow */
 import React, { useEffect, useMemo, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { shallowEqual } from 'react-redux';
-import classNames from 'classnames';
 import { useRouter } from 'next/router';
 
 import Badge, { EBadgeType } from '@components/Badge/Badge';
@@ -12,55 +11,30 @@ import ErrorMessage from '@components/ErrorMessage/ErrorMessage';
 import IconFilter from '@components/Icons/IconFilter/IconFilter';
 import LoadingContainer from '@components/LoadingContainer/LoadingContainer';
 import NamedLink from '@components/NamedLink/NamedLink';
+import SubOrderBadge from '@components/SubOrderBadge/SubOrderBadge';
 import type { TColumn } from '@components/Table/Table';
 import { TableForm } from '@components/Table/Table';
 import Tooltip from '@components/Tooltip/Tooltip';
 import { parseThousandNumber } from '@helpers/format';
 import { calculateSubOrderPrice } from '@helpers/orderHelper';
 import { useAppDispatch, useAppSelector } from '@hooks/reduxHooks';
-import { adminRoutes, partnerPaths } from '@src/paths';
+import { partnerPaths } from '@src/paths';
 import { findEndDeliveryTime, formatTimestamp } from '@src/utils/dates';
-import {
-  EOrderDraftStates,
-  EOrderStates,
-  getLabelByKey,
-  ORDER_STATES_OPTIONS,
-} from '@utils/enums';
+import { EOrderDraftStates, EOrderType } from '@utils/enums';
 import type { TObject, TTableSortValue } from '@utils/types';
 
 import type { TFilterPartnerOrderFormValues } from './components/FilterPartnerOrderForm';
 import FilterPartnerOrderForm from './components/FilterPartnerOrderForm';
-import { PartnerManageOrdersThunks } from './ManageOrders.slice';
+import {
+  PartnerManageOrdersActions,
+  PartnerManageOrdersThunks,
+} from './ManageOrders.slice';
 
 import css from './ManageOrders.module.scss';
 
-const BADGE_TYPE_BASE_ON_ORDER_STATE = {
-  [EOrderDraftStates.draft]: EBadgeType.default,
-  [EOrderDraftStates.pendingApproval]: EBadgeType.info,
-  [EOrderStates.canceled]: EBadgeType.default,
-  [EOrderStates.canceledByBooker]: EBadgeType.default,
-  [EOrderStates.completed]: EBadgeType.warning,
-  [EOrderStates.inProgress]: EBadgeType.info,
-  [EOrderStates.pendingPayment]: EBadgeType.info,
-  [EOrderStates.picking]: EBadgeType.warning,
-  [EOrderStates.reviewed]: EBadgeType.warning,
-};
-
-const BADGE_CLASS_NAME_BASE_ON_ORDER_STATE = {
-  [EOrderDraftStates.draft]: css.badgeDefault,
-  [EOrderDraftStates.pendingApproval]: css.badgeProcessing,
-  [EOrderStates.canceled]: css.badgeDefault,
-  [EOrderStates.canceledByBooker]: css.badgeDefault,
-  [EOrderStates.completed]: css.badgeSuccess,
-  [EOrderStates.inProgress]: css.badgeInProgress,
-  [EOrderStates.pendingPayment]: css.badgeProcessing,
-  [EOrderStates.picking]: css.badgeWarning,
-  [EOrderStates.reviewed]: css.badgeWarning,
-};
-
 const TABLE_COLUMN: TColumn[] = [
   {
-    key: 'title',
+    key: 'id',
     label: 'ID',
     render: ({ id, date, subOrderTitle }: any) => {
       const titleComponent = (
@@ -87,7 +61,7 @@ const TABLE_COLUMN: TColumn[] = [
     },
   },
   {
-    key: 'startDate',
+    key: 'time',
     label: 'Thời gian',
     render: (data: any) => {
       const { startDate, endDate, deliveryHour } = data;
@@ -117,7 +91,7 @@ const TABLE_COLUMN: TColumn[] = [
   {
     key: 'totalPrice',
     label: 'Tổng tiền',
-    render: ({ totalPrice }: any) => {
+    render: ({ totalPrice }: TObject) => {
       return totalPrice ? (
         <div className={css.totalPrice}>{totalPrice}</div>
       ) : (
@@ -128,18 +102,8 @@ const TABLE_COLUMN: TColumn[] = [
   {
     key: 'state',
     label: 'Trạng thái',
-    render: ({ state }: { state: EOrderStates | EOrderDraftStates }) => {
-      return (
-        <Badge
-          containerClassName={classNames(
-            css.badge,
-            BADGE_CLASS_NAME_BASE_ON_ORDER_STATE[state],
-          )}
-          labelClassName={css.badgeLabel}
-          type={BADGE_TYPE_BASE_ON_ORDER_STATE[state] || EBadgeType.default}
-          label={getLabelByKey(ORDER_STATES_OPTIONS, state)}
-        />
-      );
+    render: ({ transaction = {} }: TObject) => {
+      return <SubOrderBadge transaction={transaction} />;
     },
     sortable: true,
   },
@@ -166,7 +130,7 @@ const parseEntitiesToTableData = (subOrders: TObject[]) => {
       date,
       companyName,
       orderTitle,
-      // orderType = EOrderType.group,
+      orderType = EOrderType.group,
       staffName,
       startDate,
       endDate,
@@ -174,10 +138,12 @@ const parseEntitiesToTableData = (subOrders: TObject[]) => {
       memberOrders = {},
       restaurant,
       lineItems = [],
+      transaction,
     } = entity;
     const dayIndex = new Date(Number(date)).getDay();
 
     const { totalPrice } = calculateSubOrderPrice({
+      orderType,
       data: {
         memberOrders,
         restaurant,
@@ -202,9 +168,11 @@ const parseEntitiesToTableData = (subOrders: TObject[]) => {
         orderName: `${companyName}_${formatTimestamp(date)}`,
         staffName,
         startDate: startDate && formatTimestamp(startDate),
+        time: Number(startDate),
         endDate: endDate && formatTimestamp(endDate),
         state: EOrderDraftStates.pendingApproval,
         deliveryHour: formattedDeliveryHour,
+        transaction,
         isPaid: false,
       },
     };
@@ -218,8 +186,8 @@ const sortOrders = ({ columnName, type }: TTableSortValue, data: any) => {
   return data.sort((a: any, b: any) => {
     if (typeof a.data[columnName] === 'number') {
       return isAsc
-        ? b.data[columnName] - a.data[columnName]
-        : a.data[columnName] - b.data[columnName];
+        ? a.data[columnName] - b.data[columnName]
+        : b.data[columnName] - a.data[columnName];
     }
     if (typeof a.data[columnName] === 'string') {
       if (a.data[columnName] < b.data[columnName]) {
@@ -239,19 +207,25 @@ const ManageOrdersPage = () => {
   const router = useRouter();
   // eslint-disable-next-line @typescript-eslint/naming-convention
   const {
-    page = 1,
-    name,
-    subOrderId,
-    startTime,
-    endTime,
-    status,
-  } = router.query;
+    query: { page = 1, name, subOrderId, startTime, endTime, status },
+    isReady,
+  } = router;
   const [sortValue, setSortValue] = useState<TTableSortValue>();
-  const { queryOrderInProgress, queryOrderError, manageOrdersPagination } =
-    useAppSelector((state) => state.Order, shallowEqual);
-  const { currentSubOrders = [] } = useAppSelector(
-    (state) => state.PartnerManageOrders,
-    shallowEqual,
+
+  const isFirstLoad = useAppSelector(
+    (state) => state.PartnerManageOrders.isFirstLoad,
+  );
+  const currentSubOrders = useAppSelector(
+    (state) => state.PartnerManageOrders.currentSubOrders,
+  );
+  const pagination = useAppSelector(
+    (state) => state.PartnerManageOrders.pagination,
+  );
+  const fetchOrderInProgress = useAppSelector(
+    (state) => state.PartnerManageOrders.fetchOrderInProgress,
+  );
+  const fetchOrderError = useAppSelector(
+    (state) => state.PartnerManageOrders.fetchOrderError,
   );
   const dataTable = parseEntitiesToTableData(currentSubOrders);
 
@@ -276,18 +250,19 @@ const ManageOrdersPage = () => {
   };
 
   let content;
-  if (queryOrderInProgress) {
+  if (fetchOrderInProgress) {
     content = <LoadingContainer />;
-  } else if (queryOrderError) {
-    content = <ErrorMessage message={queryOrderError.message} />;
+  } else if (fetchOrderError) {
+    content = <ErrorMessage message={fetchOrderError.message} />;
   } else if (currentSubOrders?.length > 0) {
     content = (
       <>
         <TableForm
           columns={TABLE_COLUMN}
           data={sortedData}
-          pagination={manageOrdersPagination}
-          paginationPath={adminRoutes.ManageOrders.path}
+          pagination={pagination}
+          paginationPath={partnerPaths.ManageOrders}
+          shouldReplacePathWhenChangePage
           tableBodyCellClassName={css.bodyCell}
           tableHeadCellClassName={css.headCell}
           handleSort={handleSort}
@@ -325,12 +300,35 @@ const ManageOrdersPage = () => {
   };
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, name, subOrderId, startTime, endTime, status]);
+    if (isReady) {
+      dispatch(
+        PartnerManageOrdersActions.filterData({
+          page: Number(page),
+          ...(name ? { name } : {}),
+          ...(startTime ? { startTime } : {}),
+          ...(endTime ? { endTime } : {}),
+          ...(status ? { status } : {}),
+          ...(subOrderId ? { subOrderId } : {}),
+        }),
+      );
+    }
+  }, [isReady, page, name, subOrderId, startTime, endTime, status]);
 
   useEffect(() => {
-    dispatch(PartnerManageOrdersThunks.loadData());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    if (isFirstLoad && isReady) {
+      dispatch(PartnerManageOrdersThunks.loadData()).then(() => {
+        dispatch(
+          PartnerManageOrdersActions.filterData({
+            page: Number(page),
+            ...(name ? { name } : {}),
+            ...(startTime ? { startTime } : {}),
+            ...(endTime ? { endTime } : {}),
+            ...(status ? { status } : {}),
+            ...(subOrderId ? { subOrderId } : {}),
+          }),
+        );
+      });
+    }
   }, []);
 
   return (
