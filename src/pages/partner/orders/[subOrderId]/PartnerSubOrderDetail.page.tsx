92f28ea98a3417c86e34d5d7bffdc6e984a52408
Allen <khang.nguyen@journeyh.io>
2023-07-03 17:53:29 +0700

    PCC-46 add: function to switch view mode for suborder detail page

--- src/pages/partner/orders/[subOrderId]/PartnerSubOrderDetail.page.tsx
+++ src/pages/partner/orders/[subOrderId]/PartnerSubOrderDetail.page.tsx
@@ -1,10 +1,13 @@
-import { useEffect } from 'react';
+/* eslint-disable import/no-cycle */
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
+import RenderWhen from '@components/RenderWhen/RenderWhen';
 import { useAppDispatch } from '@hooks/reduxHooks';
 import { orderManagementThunks } from '@redux/slices/OrderManagement.slice';
 
 import SubOrderCart from './components/SubOrderCart';
+import SubOrderDetail from './components/SubOrderDetail';
 import SubOrderInfo from './components/SubOrderInfo';
 import SubOrderNote from './components/SubOrderNote';
 import SubOrderSummary from './components/SubOrderSummary';
@@ -14,17 +17,28 @@ import { PartnerSubOrderDetailThunks } from './PartnerSubOrderDetail.slice';
 import css from './PartnerSubOrderDetailPage.module.scss';
 
 type TPartnerSubOrderDetailPageProps = {};
+export enum EPartnerSubOrderDetailPage {
+  summary = 'summary',
+  detail = 'detail',
+}
 
 const PartnerSubOrderDetailPage: React.FC<
   TPartnerSubOrderDetailPageProps
 > = () => {
   const router = useRouter();
   const dispatch = useAppDispatch();
-
+  const [viewMode, setViewMode] = useState(EPartnerSubOrderDetailPage.detail);
   const {
     query: { subOrderId },
     isReady,
   } = router;
+
+  const isSummaryViewMode = viewMode === EPartnerSubOrderDetailPage.summary;
+
+  const handleChangeViewMode =
+    (_viewMode: EPartnerSubOrderDetailPage) => () => {
+      setViewMode(_viewMode);
+    };
 
   useEffect(() => {
     if (subOrderId && isReady) {
@@ -40,16 +54,22 @@ const PartnerSubOrderDetailPage: React.FC<
   return (
     <div className={css.root}>
       <SubOrderTitle />
-      <div className={css.container}>
-        <div className={css.leftPart}>
-          <SubOrderInfo />
-          <SubOrderSummary />
-          <SubOrderNote />
+      <RenderWhen condition={isSummaryViewMode}>
+        <div className={css.container}>
+          <div className={css.leftPart}>
+            <SubOrderInfo />
+            <SubOrderSummary onChangeViewMode={handleChangeViewMode} />
+            <SubOrderNote />
+          </div>
+          <div className={css.rightPart}>
+            <SubOrderCart />
+          </div>
         </div>
-        <div className={css.rightPart}>
-          <SubOrderCart />
-        </div>
-      </div>
+
+        <RenderWhen.False>
+          <SubOrderDetail onChangeViewMode={handleChangeViewMode} />
+        </RenderWhen.False>
+      </RenderWhen>
     </div>
   );
 };
