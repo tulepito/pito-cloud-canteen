b8dbacc3c7d3c4a1335f1a1a5fb617452c4fd346
Allen <khang.nguyen@journeyh.io>
2023-07-04 16:58:53 +0700

    PCC-49: add notification tooltip modal for partner header

---
+++ src/components/Layout/PartnerLayout/PartnerHeader/PartnerNotificationModal.tsx
@@ -0,0 +1,43 @@
+import { useIntl } from 'react-intl';
+
+import Button from '@components/Button/Button';
+import IconClose from '@components/Icons/IconClose/IconClose';
+
+import css from './PartnerNotificationModal.module.scss';
+
+type TPartnerNotificationModalProps = {};
+
+const PartnerNotificationModal: React.FC<
+  TPartnerNotificationModalProps
+> = () => {
+  const intl = useIntl();
+
+  const isNotificationListEmpty = true;
+  const disableMarkAllViewed = isNotificationListEmpty;
+  const handleMarkAllViewed = () => {};
+
+  return (
+    <div className={css.root}>
+      <div className={css.head}>
+        <span>
+          {intl.formatMessage({ id: 'PartnerNotificationModal.title' })}
+        </span>
+        <div className={css.closeIconWrapper}>
+          <IconClose className={css.closeIcon} />
+        </div>
+      </div>
+      <div className={css.container}></div>
+      <div className={css.bottom}>
+        <Button
+          variant="inline"
+          disabled={disableMarkAllViewed}
+          className={css.markAllViewedBtn}
+          onClick={handleMarkAllViewed}>
+          {intl.formatMessage({ id: 'PartnerNotificationModal.markAllViewed' })}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PartnerNotificationModal;
