d5961c02975f04c037d7ba2319cc6a0b2a2e1fd0
minhtranjh <minh.tran@journeyh.io>
2023-07-06 13:30:34 +0700

    fix food requirement validate

--- src/components/ListingCard/ListingDetailModal.tsx
+++ src/components/ListingCard/ListingDetailModal.tsx
@@ -211,7 +211,6 @@ const ListingDetailModal: React.FC<TListingDetailModalProps> = (props) => {
             meta={requirementField.meta}
             placeholder={fieldRequirement.placeholder}
             onChange={handleChangeRequirement}
-            validate={maxLength('Ghi chú không được vượt quá 500 kí tự', 500)}
           />
         </div>
 
