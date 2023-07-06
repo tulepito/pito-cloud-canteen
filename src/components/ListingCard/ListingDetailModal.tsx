34348176ed975246adb15353e247c9e59bc538c8
minhtranjh <minh.tran@journeyh.io>
2023-07-06 13:30:34 +0700

    fix food requirement validate

--- src/components/ListingCard/ListingDetailModal.tsx
+++ src/components/ListingCard/ListingDetailModal.tsx
@@ -51,7 +51,11 @@ const ListingDetailModal: React.FC<TListingDetailModalProps> = (props) => {
       requirement,
     },
   });
-  const requirementField = useField('requirement', foodSelectionForm);
+  const requirementField = useField(
+    'requirement',
+    foodSelectionForm,
+    maxLength('Ghi chú không được vượt quá 500 kí tự', 500),
+  );
   const packagingOptions = useAppSelector(
     (state) => state.SystemAttributes.packaging,
   );
@@ -206,7 +210,6 @@ const ListingDetailModal: React.FC<TListingDetailModalProps> = (props) => {
             meta={requirementField.meta}
             placeholder={fieldRequirement.placeholder}
             onChange={handleChangeRequirement}
-            validate={maxLength('Ghi chú không được vượt quá 500 kí tự', 500)}
           />
         </div>
 
