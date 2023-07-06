3080c21d1c3f6ceb2b3cb68f6f4b3fecd9b3ab39
Allen <khang.nguyen@journeyh.io>
2023-07-06 09:59:55 +0700

    PCC-49 update: orderUrl in partnerOrder detail template

--- src/utils/emailTemplate/partnerOrderDetailsUpdated.ts
+++ src/utils/emailTemplate/partnerOrderDetailsUpdated.ts
@@ -1,6 +1,7 @@
 import { calculatePriceQuotationInfo } from '@helpers/order/cartInfoHelper';
 
 import { formatTimestamp } from '../dates';
+import type { TObject } from '../types';
 
 const BASE_URL = process.env.NEXT_PUBLIC_CANONICAL_URL;
 
@@ -27,19 +28,24 @@ const partnerOrderDetailsUpdated = ({
   const { title: restaurantName } = restaurantListing.getAttributes();
 
   const { orderDetail: planOrderDetail } = planListing.getMetadata();
-
-  const { startDate, endDate, staffName } = orderListing.getMetadata();
+  const {
+    startDate,
+    endDate,
+    staffName,
+    orderVATPercentage = 0,
+  } = orderListing.getMetadata();
   const { companyName } = companyUser.getPublicData();
   const formattedStartDate = formatTimestamp(startDate);
-  const formattedEnddate = formatTimestamp(endDate);
+  const formattedEndDate = formatTimestamp(endDate);
 
   const { totalPrice, totalDishes, VATFee, PITOFee, totalWithVAT } =
     calculatePriceQuotationInfo({
-      planOrderDetail,
-      order: orderListing.getFullData(),
+      planOrderDetail: planOrderDetail as TObject,
+      order: orderListing.getFullData() as TObject,
+      currentOrderVATPercentage: orderVATPercentage,
     });
 
-  const orderUrl = `${BASE_URL}/partner/orders/${orderId}`;
+  const orderUrl = `${BASE_URL}/partner/orders/${orderId}_${subOrderDate}`;
 
   return `
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -1023,7 +1029,7 @@ const partnerOrderDetailsUpdated = ({
                                         color: #333333;
                                         font-size: 14px;
                                       ">
-                                      <strong>${formattedStartDate} - ${formattedEnddate}</strong>
+                                      <strong>${formattedStartDate} - ${formattedEndDate}</strong>
                                     </p>
                                   </td>
                                 </tr>
