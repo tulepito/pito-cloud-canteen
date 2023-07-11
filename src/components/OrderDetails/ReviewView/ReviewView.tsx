29422ca9d2ca04e256f5f00af1fc673ad9c43280
Allen <khang.nguyen@journeyh.io>
2023-07-11 10:50:16 +0700

    PCC-248 update: remove duplicate note section in review view

--- src/components/OrderDetails/ReviewView/ReviewView.tsx
+++ src/components/OrderDetails/ReviewView/ReviewView.tsx
@@ -110,10 +110,6 @@ const ReviewView: React.FC<TReviewViewProps> = (props) => {
           className={css.detailRoot}
           foodOrderGroupedByDate={reviewViewData.foodOrderGroupedByDate}
         />
-        <ReviewNoteSection
-          onSaveOrderNote={onSaveOrderNote}
-          data={reviewViewData.orderNoteData}
-        />
       </div>
       <div className={rightPartClasses}>
         <RenderWhen condition={!canGoBackEditMode}>
