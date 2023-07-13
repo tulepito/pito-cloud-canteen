af2fac6d5da30d7e43d043baae0c3621e985084c
minhtranjh <minh.tran@journeyh.io>
2023-07-13 11:08:10 +0700

    PCC:71 : Fix listing detail modal UI

--- src/components/OrderDetails/ReviewView/ReviewView.tsx
+++ src/components/OrderDetails/ReviewView/ReviewView.tsx
@@ -93,10 +93,12 @@ const ReviewView: React.FC<TReviewViewProps> = (props) => {
           className={css.detailRoot}
           foodOrderGroupedByDate={reviewViewData.foodOrderGroupedByDate}
         />
-        <ReviewNoteSection
-          onSaveOrderNote={onSaveOrderNote}
-          data={reviewViewData.orderNoteData}
-        />
+        {isGroupOrder && (
+          <ReviewNoteSection
+            onSaveOrderNote={onSaveOrderNote}
+            data={reviewViewData.orderNoteData}
+          />
+        )}
       </div>
       <div className={rightPartClasses}>
         <RenderWhen condition={!canGoBackEditMode}>
