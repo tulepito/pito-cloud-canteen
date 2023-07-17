afebbfe9d1ec2f51cd50a997fe8f75668188a520
Allen <khang.nguyen@journeyh.io>
2023-07-17 17:24:22 +0700

    PCC-49 fix: PCC-271

--- src/helpers/format.ts
+++ src/helpers/format.ts
@@ -4,6 +4,12 @@ export const addCommas = (num: string, separator = ',') =>
 export const removeNonNumeric = (num: string) =>
   num?.toString().replace(/[^0-9]/g, '');
 
-export const parseThousandNumber = (value: string | number) => {
-  return addCommas(removeNonNumeric(value !== null ? value?.toString() : '0'));
+export const parseThousandNumber = (
+  value: string | number,
+  separator = ',',
+) => {
+  return addCommas(
+    removeNonNumeric(value !== null ? value?.toString() : '0'),
+    separator,
+  );
 };
