97ea5feb2497682f700c0f8d90a7fb1cc24f9e5b
Allen <khang.nguyen@journeyh.io>
2023-07-21 17:32:08 +0700

    PCC-232 add: header, section title for Delivery tracking page

--- src/pages/delivery-tracking/[subOrderId]/DeliveryTracking.page.tsx
+++ src/pages/delivery-tracking/[subOrderId]/DeliveryTracking.page.tsx
@@ -1,7 +1,11 @@
 import { useEffect } from 'react';
+import { useIntl } from 'react-intl';
+import Skeleton from 'react-loading-skeleton';
 import { useRouter } from 'next/router';
 
-import { useAppDispatch } from '@hooks/reduxHooks';
+import BasicHeader from '@components/BasicHeader/BasicHeader';
+import RenderWhen from '@components/RenderWhen/RenderWhen';
+import { useAppDispatch, useAppSelector } from '@hooks/reduxHooks';
 
 import { DeliveryTrackingPageThunks } from './DeliveryTrackingPage.slice';
 
@@ -10,8 +14,12 @@ import css from './DeliveryTrackingPage.module.scss';
 type TDeliveryTrackingPageProps = {};
 
 const DeliveryTrackingPage: React.FC<TDeliveryTrackingPageProps> = () => {
+  const intl = useIntl();
   const router = useRouter();
   const dispatch = useAppDispatch();
+  const loadDataInProgress = useAppSelector(
+    (state) => state.DeliveryTrackingPage.loadDataInProgress,
+  );
 
   const {
     query: { subOrderId = '' },
@@ -29,7 +37,31 @@ const DeliveryTrackingPage: React.FC<TDeliveryTrackingPageProps> = () => {
     );
   }, []);
 
-  return <div className={css.root}></div>;
+  return (
+    <div className={css.root}>
+      <BasicHeader />
+
+      <div className={css.titleContainer}>
+        <div className={css.title}>
+          {intl.formatMessage({ id: 'DeliveryTrackingPage.title' })}
+        </div>
+        <RenderWhen condition={loadDataInProgress}>
+          <Skeleton className={css.subTitleLoading} />
+
+          <RenderWhen.False>
+            <div>
+              {intl.formatMessage(
+                { id: 'DeliveryTrackingPage.subTitle' },
+                { orderTitle: '' },
+              )}
+            </div>
+          </RenderWhen.False>
+        </RenderWhen>
+      </div>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default DeliveryTrackingPage;
