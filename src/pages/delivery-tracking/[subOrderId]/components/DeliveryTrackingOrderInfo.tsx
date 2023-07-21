40762724344e1749ef1f22ab4234c90b0192a444
Allen <khang.nguyen@journeyh.io>
2023-07-21 17:46:21 +0700

    PCC-232 add: text translation for delivery tracking page

---
+++ src/pages/delivery-tracking/[subOrderId]/components/DeliveryTrackingOrderInfo.tsx
@@ -0,0 +1,11 @@
+import css from './DeliveryTrackingOrderInfo.module.scss';
+
+type TDeliveryTrackingOrderInfoProps = {};
+
+const DeliveryTrackingOrderInfo: React.FC<
+  TDeliveryTrackingOrderInfoProps
+> = () => {
+  return <div className={css.root}></div>;
+};
+
+export default DeliveryTrackingOrderInfo;
