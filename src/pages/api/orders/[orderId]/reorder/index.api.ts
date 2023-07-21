e2ea28e40aef7138ed866744a024076ecdf300f2
minhtranjh <minh.tran@journeyh.io>
2023-07-21 16:24:38 +0700

    reorder api

---
+++ src/pages/api/orders/[orderId]/reorder/index.api.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { HttpMethod } from '@apis/configs';
+import orderServices from '@pages/api/apiServices/order/index.service';
+import cookies from '@services/cookie';
+import { getCurrentUser, handleError } from '@services/sdk';
+import { User } from '@src/utils/data';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  try {
+    const apiMethod = req.method;
+
+    switch (apiMethod) {
+      case HttpMethod.POST: {
+        const { orderId } = req.query;
+        const { currentUser: booker } = await getCurrentUser(req, res);
+        const response = await orderServices.reorder(
+          orderId as string,
+          User(booker).getId(),
+        );
+
+        return res.status(200).json(response);
+      }
+      case HttpMethod.GET:
+      case HttpMethod.DELETE:
+      case HttpMethod.PUT:
+      default:
+        return res.status(200).json('Method is not allowed');
+    }
+  } catch (error) {
+    console.error(error);
+    handleError(res, error);
+  }
+}
+
+export default cookies(handler);
