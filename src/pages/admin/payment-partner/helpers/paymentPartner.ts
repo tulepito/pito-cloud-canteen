37fe24daecf8e0f3feceaa3a0789c1bfa02d53b4
ChuongNguyenJH <chuong.nguyen@journeyh.io>
2023-07-26 09:35:03 +0700

    PCC-222 add filter& download function

--- src/pages/admin/payment-partner/helpers/paymentPartner.ts
+++ src/pages/admin/payment-partner/helpers/paymentPartner.ts
@@ -1,3 +1,7 @@
+import * as XLSX from 'xlsx';
+
+import { parseThousandNumber } from '@helpers/format';
+import { formatTimestamp } from '@src/utils/dates';
 import type { TObject } from '@src/utils/types';
 
 export const filterPaymentPartner = (
@@ -19,4 +23,46 @@ export const filterPaymentPartner = (
   };
 
   return paymentRecords.filter(filterFn);
+};
+
+export const parseEntitiesToExportCsv = (paymentRecords: any[]) => {
+  const exportedData = paymentRecords.map((paymentRecord) => {
+    const {
+      orderTitle,
+      partnerName,
+      subOrderDate,
+      companyName,
+      deliveryHour,
+      totalAmount,
+      paidAmount,
+      remainAmount,
+      status,
+    } = paymentRecord.data || {};
+
+    return {
+      ID: orderTitle,
+      'Đối tác': partnerName,
+      'Tên đơn hàng': `${companyName}_${formatTimestamp(subOrderDate)}`,
+      'Thời gian': `${deliveryHour} ${formatTimestamp(subOrderDate)}`,
+      'Tổng giá trị': parseThousandNumber(totalAmount),
+      'Đã thanh toán': parseThousandNumber(paidAmount),
+      'Còn lại': parseThousandNumber(remainAmount),
+      'Trạng thái': status === 'isPaid' ? 'Đã thanh toán' : 'Chưa thanh toán',
+    };
+  });
+
+  return exportedData;
+};
+
+export const makeExcelFile = (data: any) => {
+  const paymentRecordToExport = parseEntitiesToExportCsv(data);
+  const ws = XLSX.utils.json_to_sheet(paymentRecordToExport);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  XLSX.writeFile(
+    wb,
+    `${formatTimestamp(
+      new Date().getTime(),
+    )}_Danh_sách_thanh_toán_đối_tác.xlsx`,
+  );
 };
