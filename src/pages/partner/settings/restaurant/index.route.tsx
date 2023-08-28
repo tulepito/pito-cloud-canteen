d7ece63ef6a0221273f1aa7eebc9199d2b44bd4b
Allen <khang.nguyen@journeyh.io>
2023-08-28 07:09:56 +0700

    PCC-422 update: logic toggle partner app status

--- src/pages/partner/settings/restaurant/index.route.tsx
+++ src/pages/partner/settings/restaurant/index.route.tsx
@@ -3,10 +3,13 @@ import { useRouter } from 'next/router';
 import MetaWrapper from '@components/MetaWrapper/MetaWrapper';
 import { partnerPaths } from '@src/paths';
 
+import { useFetchPartnerListing } from '../hooks/useFetchPartnerListing';
+
 import RestaurantSettingModal from './components/RestaurantSettingModal';
 
 const PartnerRestaurantSettingRoute = () => {
   const router = useRouter();
+  useFetchPartnerListing();
 
   const handleClose = () => {
     router.push(partnerPaths.Settings);
