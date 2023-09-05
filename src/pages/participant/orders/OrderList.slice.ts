d8b4b51f4535c9a6a3eb2bd618a12ef03fdad60b
ChuongNguyenJH <chuong.nguyen@journeyh.io>
2023-09-05 18:02:47 +0700

    PCC-580 fix recommend foods

--- src/pages/participant/orders/OrderList.slice.ts
+++ src/pages/participant/orders/OrderList.slice.ts
@@ -212,7 +212,7 @@ export const recommendFood = ({
       intersection(
         Listing(food)
           .getPublicData()
-          .allergicIngredients.map((_foodAllergy: string) =>
+          .allergicIngredients?.map((_foodAllergy: string) =>
             toNonAccentVietnamese(_foodAllergy),
           ),
         allergies.map((allergy: string) =>
@@ -220,7 +220,6 @@ export const recommendFood = ({
         ),
       ).length === 0,
   );
-
   const isAllFoodHaveAllergies = filteredFoodListByAllergies.length === 0;
 
   const foodListToFilter = isAllFoodHaveAllergies
