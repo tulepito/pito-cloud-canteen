19e266061b0e8296a32fd62ca1baf8b5a884aecc
Allen <khang.nguyen@journeyh.io>
2023-09-15 17:23:20 +0700

    update: logic query all company members

--- src/pages/api/company/all-employees.api.ts
+++ src/pages/api/company/all-employees.api.ts
@@ -1,25 +1,40 @@
+import isEmpty from 'lodash/isEmpty';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import cookies from '@services/cookie';
+import { composeApiCheckers } from '@apis/configs';
+import { EHttpStatusCode } from '@apis/errors';
+import { queryAllPages } from '@helpers/apiHelpers';
 import { getIntegrationSdk } from '@services/integrationSdk';
-import { denormalisedResponseEntities } from '@utils/data';
+import companyChecker from '@services/permissionChecker/company';
 import { EImageVariants } from '@utils/enums';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   const { companyId } = req.query;
-  const integrationSdk = getIntegrationSdk();
-  const allMembersResponse = denormalisedResponseEntities(
-    await integrationSdk.users.query({
-      meta_companyList: `has_all:${companyId}`,
-      include: ['profileImage'],
-      'fields.image': [
-        `variants.${EImageVariants.squareSmall}`,
-        `variants.${EImageVariants.squareSmall2x}`,
-      ],
-    }),
-  );
 
-  res.json(allMembersResponse);
+  try {
+    if (isEmpty(companyId)) {
+      return res.status(EHttpStatusCode.BadRequest).json({
+        error: 'Missing companyId',
+      });
+    }
+    const integrationSdk = getIntegrationSdk();
+
+    const allMembersResponse = await queryAllPages({
+      sdkModel: integrationSdk.users,
+      query: {
+        meta_companyList: `has_all:${companyId}`,
+        include: ['profileImage'],
+        'fields.image': [
+          `variants.${EImageVariants.squareSmall}`,
+          `variants.${EImageVariants.squareSmall2x}`,
+        ],
+      },
+    });
+
+    return res.json(allMembersResponse);
+  } catch (error) {
+    console.error('Error query all company members, company ID: ', companyId);
+  }
 }
 
-export default cookies(handler);
+export default composeApiCheckers(companyChecker)(handler);
