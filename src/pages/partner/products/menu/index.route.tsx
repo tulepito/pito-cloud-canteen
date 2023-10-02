a542e5c33a754e7c6ab37a4090732dfa178471ee
Allen <khang.nguyen@journeyh.io>
2023-10-02 14:54:03 +0700

    PCC-432 add: function navigate to create menu page on partner manage menus page

--- src/pages/partner/products/menu/index.route.tsx
+++ src/pages/partner/products/menu/index.route.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import isEmpty from 'lodash/isEmpty';
+import { useRouter } from 'next/router';
 
 import MetaWrapper from '@components/MetaWrapper/MetaWrapper';
 import { useAppSelector } from '@hooks/reduxHooks';
+import { partnerPaths } from '@src/paths';
 
 import ProductLayout from '../ProductLayout';
 
 import PartnerManageMenusPage from './PartnerManageMenus.page';
 
 const PartnerManageMenusRoute = () => {
+  const router = useRouter();
   const queryMenusInProgress = useAppSelector(
     (state) => state.PartnerManageMenus.fetchMenusInProgress,
   );
@@ -17,12 +20,16 @@ const PartnerManageMenusRoute = () => {
   const shouldHideAddMenuButton =
     queryMenusInProgress || (!queryMenusInProgress && isEmpty(menus));
 
+  const handleNavigateToCreateMenuPage = () => {
+    router.push(partnerPaths.CreateMenu);
+  };
+
   return (
     <MetaWrapper routeName="PartnerManageMenusRoute">
       <ProductLayout
         currentPage="menu"
         shouldHideAddProductButton={shouldHideAddMenuButton}
-        handleAddProduct={() => {}}>
+        handleAddProduct={handleNavigateToCreateMenuPage}>
         <PartnerManageMenusPage />
       </ProductLayout>
     </MetaWrapper>
