c01abcbfbdac9bc97c1912decd4be7b78237a291
Allen <khang.nguyen@journeyh.io>
2023-10-02 09:46:55 +0700

    PCC-432 update: partner menu card UI

---
+++ src/pages/partner/products/menu/components/MenuCard.tsx
@@ -0,0 +1,90 @@
+import { useField, useForm } from 'react-final-form-hooks';
+
+import Badge from '@components/Badge/Badge';
+import IconArrow from '@components/Icons/IconArrow/IconArrow';
+import IconClock from '@components/Icons/IconClock/IconClock';
+import IconDelete from '@components/Icons/IconDelete/IconDelete';
+import IconEdit from '@components/Icons/IconEdit/IconEdit';
+import RenderWhen from '@components/RenderWhen/RenderWhen';
+import Toggle from '@components/Toggle/Toggle';
+import { Listing } from '@src/utils/data';
+import { formatTimestamp } from '@src/utils/dates';
+import { EListingStates } from '@src/utils/enums';
+import type { TListing } from '@src/utils/types';
+
+import css from './MenuCard.module.scss';
+
+type TMenuCardProps = {
+  menu: TListing;
+};
+
+const MenuCard: React.FC<TMenuCardProps> = ({ menu }) => {
+  const menuGetter = Listing(menu);
+  const { title: menuName } = menuGetter.getAttributes();
+  const { startDate, endDate } = menuGetter.getPublicData();
+  const { listingState = EListingStates.draft } = menuGetter.getMetadata();
+
+  const isDraftMenu = listingState === EListingStates.draft;
+
+  const today = new Date().getTime();
+  const isInvalidTimeRangeMenu = endDate >= today;
+  const shouldShowActiveMenuToggle =
+    isInvalidTimeRangeMenu &&
+    [EListingStates.published, EListingStates.closed].includes(listingState);
+
+  const { form } = useForm({
+    initialValues: {
+      isActive: listingState === EListingStates.published,
+    },
+    onSubmit: () => {},
+  });
+  const isActiveField = useField(`isActive`, form);
+
+  const formattedMenuValidTimeRange = `${formatTimestamp(
+    startDate,
+  )} - ${formatTimestamp(endDate)}`;
+
+  return (
+    <div className={css.root}>
+      <div className={css.titleContainer}>
+        <div>{menuName}</div>
+        <IconArrow className={css.arrowIcon} direction="right" />
+      </div>
+      <div className={css.infoContainer}>
+        <div className={css.timeContainer}>
+          <IconClock />
+
+          <div>{formattedMenuValidTimeRange}</div>
+        </div>
+        <RenderWhen condition={isDraftMenu}>
+          <Badge label="Nháp" />
+          <RenderWhen.False>
+            <RenderWhen condition={isInvalidTimeRangeMenu}>
+              <Badge label="Menu đã hết hạn" />
+            </RenderWhen>
+          </RenderWhen.False>
+        </RenderWhen>
+      </div>
+      <div className={css.actionContainer}>
+        <div className={css.iconContainer}>
+          <IconEdit />
+        </div>
+        <div className={css.iconContainer}>
+          <IconDelete />
+        </div>
+        <RenderWhen condition={shouldShowActiveMenuToggle}>
+          <Toggle
+            id={'MealDateForm.orderType'}
+            status={isActiveField.input.value ? 'on' : 'off'}
+            className={css.isActiveField}
+            onClick={(value) => {
+              isActiveField.input.onChange(value);
+            }}
+          />
+        </RenderWhen>
+      </div>
+    </div>
+  );
+};
+
+export default MenuCard;
