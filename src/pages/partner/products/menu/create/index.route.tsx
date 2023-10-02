030745875b45762d34d660ee12644294974fce5f
Allen <khang.nguyen@journeyh.io>
2023-10-02 12:01:04 +0700

    PCC-432 add: init route for partner create menu

---
+++ src/pages/partner/products/menu/create/index.route.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MetaWrapper from '@components/MetaWrapper/MetaWrapper';
+
+const PartnerCreateMenuRoute = () => {
+  return (
+    <MetaWrapper routeName="PartnerCreateMenuRoute">
+      PartnerCreateMenuRoute
+    </MetaWrapper>
+  );
+};
+
+export default PartnerCreateMenuRoute;
