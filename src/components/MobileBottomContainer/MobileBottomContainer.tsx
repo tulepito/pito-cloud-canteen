08a42d876bb06f6b8c9d55e430ee0fd4096d9de8
Allen <khang.nguyen@journeyh.io>
2023-10-20 17:30:43 +0700

    PCC-688 update: mobile bottom container prop types

--- src/components/MobileBottomContainer/MobileBottomContainer.tsx
+++ src/components/MobileBottomContainer/MobileBottomContainer.tsx
@@ -1,19 +1,20 @@
+import type { PropsWithChildren } from 'react';
 import classNames from 'classnames';
 
 import type { TDefaultProps } from '@src/utils/types';
 
 import css from './MobileBottomContainer.module.scss';
 
-type TMobileBottomContainerProps = {} & TDefaultProps;
+type TMobileBottomContainerProps = PropsWithChildren<TDefaultProps>;
 
 const MobileBottomContainer: React.FC<TMobileBottomContainerProps> = (
   props,
 ) => {
-  const { rootClassName, className } = props;
+  const { rootClassName, className, children } = props;
 
   const rootClasses = classNames(rootClassName || css.root, className);
 
-  return <div className={rootClasses}></div>;
+  return <div className={rootClasses}>{children}</div>;
 };
 
 export default MobileBottomContainer;
