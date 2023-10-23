d44e1a33d71173ecbbde6599bfcfc5f20c1676d0
Allen <khang.nguyen@journeyh.io>
2023-10-23 13:43:16 +0700

    PCC-1078 add: config route for admin edit order page

---
+++ src/pages/admin/order/edit/[orderId]/index.route.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import MetaWrapper from '@components/MetaWrapper/MetaWrapper';
+
+export default function AdminEditOrderRoute() {
+  return <MetaWrapper routeName="AdminEditOrderRoute"></MetaWrapper>;
+}
