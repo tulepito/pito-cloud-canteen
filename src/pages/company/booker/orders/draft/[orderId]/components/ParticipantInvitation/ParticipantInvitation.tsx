17aa078a02a00181d8f464276b3a980317540b65
Allen <khang.nguyen@journeyh.io>
2023-11-09 15:11:39 +0700

    PCC-1166 add: title section for booker draft Invite participant

--- src/pages/company/booker/orders/draft/[orderId]/components/ParticipantInvitation/ParticipantInvitation.tsx
+++ src/pages/company/booker/orders/draft/[orderId]/components/ParticipantInvitation/ParticipantInvitation.tsx
@@ -1,3 +1,7 @@
+import Badge from '@components/Badge/Badge';
+import Button from '@components/Button/Button';
+import IconArrow from '@components/Icons/IconArrow/IconArrow';
+
 import css from './ParticipantInvitation.module.scss';
 
 type TParticipantInvitationProps = {
@@ -5,8 +9,32 @@ type TParticipantInvitationProps = {
   onPublishOrder: () => void;
 };
 
-const ParticipantInvitation: React.FC<TParticipantInvitationProps> = () => {
-  return <div className={css.root}>ParticipantInvitation</div>;
+const ParticipantInvitation: React.FC<TParticipantInvitationProps> = ({
+  onGoBack,
+}) => {
+  return (
+    <div className={css.root}>
+      <div className={css.contentContainer}>
+        <div className={css.titleContainer}>
+          <Button
+            variant="inline"
+            className={css.goBackContainer}
+            onClick={onGoBack}>
+            <IconArrow direction="left" />
+            <span className={css.goBackText}>Quay lại</span>
+          </Button>
+          <div className={css.title}>
+            <div>Mời thành viên tham gia nhóm</div>
+            <Badge label="Các thành viên được mời sẽ nhận thông báo chọn món ngay sau khi gửi lời mời" />
+          </div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ParticipantInvitation;
