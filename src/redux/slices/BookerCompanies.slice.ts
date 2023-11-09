6d1f5326188820c080f033d668689799de4c04ce
Allen <khang.nguyen@journeyh.io>
2023-11-09 11:26:30 +0700

    PCC-1159 update: BookerCompaniesThunks.fetchBookerCompanies
    - call: QuizThunks.queryCompanyOrders

--- src/redux/slices/BookerCompanies.slice.ts
+++ src/redux/slices/BookerCompanies.slice.ts
@@ -2,9 +2,10 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { getCompaniesApi } from '@apis/index';
 import { createAsyncThunk } from '@redux/redux.helper';
+import { User } from '@src/utils/data';
 import type { TUser } from '@src/utils/types';
 
-import { QuizActions } from './Quiz.slice';
+import { QuizActions, QuizThunks } from './Quiz.slice';
 
 // ================ Initial states ================ //
 type TBookerCompaniesState = {
@@ -22,8 +23,13 @@ const fetchBookerCompanies = createAsyncThunk(
   FETCH_BOOKER_COMPANIES,
   async (_, { dispatch }) => {
     const { data: response } = await getCompaniesApi();
-    const [company] = response || [];
-    dispatch(QuizActions.setSelectedCompany(company));
+    const [firstCompanyMaybe] = response || [];
+    const firstCompanyId = User(firstCompanyMaybe).getId();
+    dispatch(QuizActions.setSelectedCompany(firstCompanyMaybe));
+
+    if (firstCompanyId) {
+      dispatch(QuizThunks.queryCompanyOrders(firstCompanyId));
+    }
 
     return response;
   },
