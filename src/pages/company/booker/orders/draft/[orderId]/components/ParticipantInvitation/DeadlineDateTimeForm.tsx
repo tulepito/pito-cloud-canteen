52c73b3e08979d99fd6271b5fcb4dcf8aba75cc5
Allen <khang.nguyen@journeyh.io>
2023-11-09 15:54:34 +0700

    PCC-1166 add: deadline date time form for Invite participant (booker draft order page)

---
+++ src/pages/company/booker/orders/draft/[orderId]/components/ParticipantInvitation/DeadlineDateTimeForm.tsx
@@ -0,0 +1,96 @@
+import type { FormProps, FormRenderProps } from 'react-final-form';
+import { Form as FinalForm } from 'react-final-form';
+import { DateTime } from 'luxon';
+
+import Button from '@components/Button/Button';
+import Form from '@components/Form/Form';
+import FieldDatePicker from '@components/FormFields/FieldDatePicker/FieldDatePicker';
+import FieldDropdownSelect from '@components/FormFields/FieldDropdownSelect/FieldDropdownSelect';
+import IconClock from '@components/Icons/IconClock/IconClock';
+import { useAppSelector } from '@hooks/reduxHooks';
+import { TimeOptions } from '@utils/dates';
+
+import css from './DeadlineDateTimeForm.module.scss';
+
+export type DeadlineDateTimeFormValues = {
+  deadlineDate: string | number;
+  deadlineHour: string;
+};
+
+type TExtraProps = { deliveryTime: Date; shouldDisableSubmit?: boolean };
+type DeadlineDateTimeFormComponentProps =
+  FormRenderProps<DeadlineDateTimeFormValues> & Partial<TExtraProps>;
+type DeadlineDateTimeFormProps = FormProps<DeadlineDateTimeFormValues> &
+  TExtraProps;
+
+const DeadlineDateTimeFormComponent: React.FC<
+  DeadlineDateTimeFormComponentProps
+> = (props) => {
+  const {
+    deliveryTime,
+    shouldDisableSubmit = false,
+    values,
+    invalid,
+    handleSubmit,
+  } = props;
+  const bookerPublishOrderInProgress = useAppSelector(
+    (state) => state.Order.bookerPublishOrderInProgress,
+  );
+
+  const { deadlineDate: deadlineDateFormFormValues } = values;
+
+  const selectedDeadlineDate = deadlineDateFormFormValues
+    ? new Date(Number(deadlineDateFormFormValues))
+    : DateTime.fromMillis(Number(Date.now())).plus({ days: 2 }).toJSDate();
+  const minDeadlineDate = DateTime.fromJSDate(new Date())
+    .plus({ days: 1 })
+    .toJSDate();
+  const maxDeadlineDate = DateTime.fromJSDate(deliveryTime!)
+    .minus({ days: 2 })
+    .toJSDate();
+
+  const anyActionsInProgress = bookerPublishOrderInProgress;
+  const submitDisabled = anyActionsInProgress || shouldDisableSubmit || invalid;
+  const submitInProgress = anyActionsInProgress;
+
+  return (
+    <Form className={css.formRoot} onSubmit={handleSubmit}>
+      <div className={css.formContainer}>
+        <div className={css.formTitle}>Thời hạn kết thúc chọn món</div>
+        <FieldDatePicker
+          id="DeadlineDateTimeForm.deadlineDate"
+          name="deadlineDate"
+          selected={selectedDeadlineDate}
+          label="Chọn ngày hết hạn"
+          minDate={minDeadlineDate}
+          maxDate={maxDeadlineDate}
+          dateFormat={'EEE, dd MMMM, yyyy'}
+          placeholderText={'Ngày hết hạn'}
+          className={css.dateInput}
+          autoComplete="off"
+        />
+        <FieldDropdownSelect
+          id="DeadlineDateTimeForm.deadlineHour"
+          name="deadlineHour"
+          placeholderText="Giờ hết hạn"
+          className={css.fieldSelect}
+          label={'Chọn giờ hết hạn'}
+          leftIcon={<IconClock />}
+          options={TimeOptions}
+        />
+        <Button
+          variant="cta"
+          disabled={submitDisabled}
+          inProgress={submitInProgress}>
+          Gửi lời mời qua email
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+const DeadlineDateTimeForm: React.FC<DeadlineDateTimeFormProps> = (props) => {
+  return <FinalForm {...props} component={DeadlineDateTimeFormComponent} />;
+};
+
+export default DeadlineDateTimeForm;
--- src/pages/company/booker/orders/draft/[orderId]/components/ParticipantInvitation/ParticipantInvitation.tsx
+++ src/pages/company/booker/orders/draft/[orderId]/components/ParticipantInvitation/ParticipantInvitation.tsx
@@ -1,10 +1,14 @@
+import { DateTime } from 'luxon';
+
 import Badge from '@components/Badge/Badge';
 import Button from '@components/Button/Button';
 import IconArrow from '@components/Icons/IconArrow/IconArrow';
 import AlertModal from '@components/Modal/AlertModal';
+import { useAppSelector } from '@hooks/reduxHooks';
 import useBoolean from '@hooks/useBoolean';
+import { Listing } from '@src/utils/data';
 
-import DeliveryDateTimeForm from './DeliveryDateTimeForm';
+import DeadlineDateTimeForm from './DeadlineDateTimeForm';
 
 import css from './ParticipantInvitation.module.scss';
 
@@ -15,9 +19,35 @@ type TParticipantInvitationProps = {
 
 const ParticipantInvitation: React.FC<TParticipantInvitationProps> = ({
   onGoBack,
+  onPublishOrder,
 }) => {
   const confirmPublishOrderControl = useBoolean();
-  const handleSubmitPublishOrder = () => {};
+  const order = useAppSelector((state) => state.Order.order);
+
+  const { startDate, deadlineDate, deadlineHour } =
+    Listing(order).getMetadata();
+
+  // * calculate start delivery time
+  const nextStartWeek = DateTime.fromJSDate(new Date())
+    .startOf('week')
+    .startOf('day')
+    .plus({ days: 7 })
+    .toMillis();
+  const deliveryTime = new Date(startDate || nextStartWeek);
+
+  // * prepare deadline date time form initial
+  const defaultDeadlineDate = DateTime.fromMillis(startDate || nextStartWeek)
+    .minus({ days: 2 })
+    .toMillis();
+  const deadlineDateTimeInitialValues = {
+    deadlineDate: new Date(deadlineDate).getTime() || defaultDeadlineDate,
+    deadlineHour: deadlineHour || '06:30',
+  };
+
+  const handleSubmitPublishOrder = () => {
+    confirmPublishOrderControl.setFalse();
+    onPublishOrder();
+  };
 
   return (
     <div className={css.root}>
@@ -35,15 +65,17 @@ const ParticipantInvitation: React.FC<TParticipantInvitationProps> = ({
             <Badge label="Các thành viên được mời sẽ nhận thông báo chọn món ngay sau khi gửi lời mời" />
           </div>
         </div>
-        <div>
+        <div className={css.participantInvitationContainer}>
           <div>
-            <DeliveryDateTimeForm
+            <DeadlineDateTimeForm
+              deliveryTime={deliveryTime}
+              initialValues={deadlineDateTimeInitialValues}
               onSubmit={confirmPublishOrderControl.setTrue}
             />
             <AlertModal
               childrenClassName={css.confirmModalChildrenContainer}
               isOpen={confirmPublishOrderControl.value}
-              handleClose={onGoBack}
+              handleClose={confirmPublishOrderControl.setFalse}
               title="Xác nhận đơn và gửi lời mời"
               cancelLabel="Đóng"
               confirmLabel={'Gửi lời mời'}
