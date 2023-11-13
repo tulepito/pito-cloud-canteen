7b602633d7b30a52310a1714b83b3a419f3af793
Allen <khang.nguyen@journeyh.io>
2023-11-13 13:31:26 +0700

    PCC-1166 update: icon attributes

--- src/components/Icons/IconLightBulb/IconLightBulb.tsx
+++ src/components/Icons/IconLightBulb/IconLightBulb.tsx
@@ -22,9 +22,9 @@ const IconLightBulb: React.FC<TIconLightBulbProps> = (props) => {
       <path
         d="M21 2L20 3M3 2L4 3M21 16L20 15M3 16L4 15M9 18H15M10 21H14M12 3C8 3 5.952 4.95 6 8C6.023 9.487 6.5 10.5 7.5 11.5C8.5 12.5 9 13 9 15H15C15 13 15.5 12.5 16.5 11.5C17.5 10.5 17.977 9.487 18 8C18.048 4.95 16 3 12 3Z"
         stroke="#262626"
-        stroke-width="1.8"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   ) : null;
