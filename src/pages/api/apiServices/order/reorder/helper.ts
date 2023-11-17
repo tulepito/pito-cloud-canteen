07b93b7258956f034132cf14471be99aeb64acb4
Allen <khang.nguyen@journeyh.io>
2023-11-17 13:55:01 +0700

    PCC-1151: update logic reorder
    - Check new foodList from menu for same week day (with old order)
    - Find new menu for random restaurant for different week day (with old order)

---
+++ src/pages/api/apiServices/order/reorder/helper.ts
@@ -0,0 +1,247 @@
+import { isEmpty, omit } from 'lodash';
+
+import type { TDaySession } from '@components/CalendarDashboard/helpers/types';
+import { isOrderDetailDatePickedFood } from '@helpers/orderHelper';
+import { denormalisedResponseEntities } from '@services/data';
+import { fetchListing } from '@services/integrationHelper';
+import { Listing } from '@src/utils/data';
+import { getDaySessionFromDeliveryTime } from '@src/utils/dates';
+import { EListingStates } from '@src/utils/enums';
+import { INITIAL_DELIVERY_TIME_BASE_ON_DAY_SESSION } from '@src/utils/options';
+import type { TObject } from '@src/utils/types';
+
+import { checkRestaurantAvailableStatus } from '../checkRestaurantAvailableStatus';
+import {
+  recommendRestaurantForSpecificDay,
+  recommendRestaurants,
+} from '../recommendRestaurants/index.services';
+import { prepareMenuFoodList } from '../recommendRestaurants/prepareData';
+
+export const normalizeOrderMetadata = (metadata: TObject, newData: TObject) => {
+  const {
+    companyId,
+    vatSettings = {},
+    serviceFees = {},
+    listingType,
+    dayInWeek,
+    deliveryAddress,
+    detailAddress,
+    displayedDurationTime,
+    durationTimeMode,
+    memberAmount,
+    notes,
+    nutritions,
+    orderType,
+    orderVATPercentage,
+    packagePerMember,
+    participants,
+    pickAllow,
+    selectedGroups,
+    shipperName,
+    staffName,
+    vatAllow,
+    deliveryHour,
+    daySession,
+    mealType,
+    deadlineHour,
+  } = metadata || {};
+  const { daySession: newDaySession } = newData || {};
+
+  const oldDaySession =
+    daySession ||
+    getDaySessionFromDeliveryTime(
+      isEmpty(deliveryHour)
+        ? undefined
+        : deliveryHour.includes('-')
+        ? deliveryHour.split('-')[0]
+        : deliveryHour,
+    );
+  const newDeliveryHour =
+    newDaySession === oldDaySession
+      ? deliveryHour
+      : INITIAL_DELIVERY_TIME_BASE_ON_DAY_SESSION[newDaySession as TDaySession];
+
+  const newOrderMetadata = {
+    companyId,
+    vatSettings,
+    serviceFees,
+    listingType,
+    dayInWeek,
+    deliveryAddress,
+    detailAddress,
+    displayedDurationTime,
+    durationTimeMode,
+    memberAmount,
+    notes,
+    nutritions,
+    orderType,
+    orderVATPercentage,
+    packagePerMember,
+    participants,
+    pickAllow,
+    selectedGroups,
+    shipperName,
+    staffName,
+    vatAllow,
+    deliveryHour: newDeliveryHour,
+    daySession: newDaySession,
+    deadlineHour,
+    mealType,
+  };
+
+  return newOrderMetadata;
+};
+
+const ORDER_DETAIL_KEYS_TO_REMOVE = [
+  'isPaid',
+  'lastTransition',
+  'transactionId',
+];
+
+export const createNewPlanFromOldPlans = async ({
+  newOrderId,
+  newOrder,
+  generatedOrderTitle,
+  subAccountId,
+  isCreatedByAdmin,
+  oldPlans,
+  integrationSdk,
+  orderDatesInTimestamp,
+  orderDatesWeekdayList,
+  oldOrderDatesInTimestamp,
+  oldOrderDatesWeekdayList,
+  isGroupOrder,
+  initialMemberOrder,
+  oldOrderMetadata,
+}: TObject) => {
+  return Promise.all(
+    oldPlans.map(async (id: string, index: number) => {
+      const [oldPlan] = denormalisedResponseEntities(
+        await integrationSdk.listings.show({ id }),
+      );
+      const oldPlanMetadata = Listing(oldPlan).getMetadata();
+      const { orderDetail: oldOrderDetail = {} } = oldPlanMetadata;
+
+      // * find has restaurant & food from old order detail
+      const hasRestaurantDateListFormOldPlan = Object.keys(
+        oldOrderDetail,
+      ).filter((date) => isOrderDetailDatePickedFood(oldOrderDetail[date]));
+      const hasRestaurantDatesCount = hasRestaurantDateListFormOldPlan.length;
+
+      let newOrderDetail: TObject = {};
+
+      orderDatesInTimestamp.forEach((currentDate: number, dateIdx: number) => {
+        console.debug(
+          '💫 > orderDatesInTimestamp.forEach > currentDate: ',
+          currentDate,
+        );
+        const weekDayOfDate = orderDatesWeekdayList[dateIdx];
+        let dateToCopy: string | number =
+          oldOrderDatesInTimestamp[
+            oldOrderDatesWeekdayList.indexOf(weekDayOfDate)
+          ];
+
+        // * if week day not include in old week day list or empty order detail on date
+        if (!dateToCopy || isEmpty(oldOrderDetail[dateToCopy])) {
+          if (hasRestaurantDatesCount === 0) {
+            return;
+          }
+
+          dateToCopy =
+            hasRestaurantDateListFormOldPlan[
+              Math.floor(Math.random() * (hasRestaurantDatesCount - 1))
+            ];
+        }
+
+        // * remove unnecessary info from old order detail on date
+        const subOrderNeededData = omit(
+          oldOrderDetail[dateToCopy],
+          ORDER_DETAIL_KEYS_TO_REMOVE,
+        );
+
+        newOrderDetail[`${currentDate}`] = {
+          ...subOrderNeededData,
+          memberOrders: isGroupOrder ? {} : initialMemberOrder,
+        };
+      });
+
+      // * check available status
+      const restaurantAvailableMap = await checkRestaurantAvailableStatus(
+        newOrder,
+        newOrderDetail,
+      );
+      const timestampList = Object.keys(restaurantAvailableMap);
+
+      const unAvailableTimestampList = Object.keys(
+        restaurantAvailableMap,
+      ).filter((item) => !restaurantAvailableMap[item]);
+
+      const isAllDayUnavailable =
+        unAvailableTimestampList.length === timestampList.length;
+
+      if (isAllDayUnavailable) {
+        newOrderDetail = await recommendRestaurants({
+          orderId: newOrderId,
+          shouldCalculateDistance: !isCreatedByAdmin,
+        });
+      } else if (unAvailableTimestampList.length !== 0) {
+        await Promise.all(
+          timestampList.map(async (timestamp) => {
+            const isAvailable = restaurantAvailableMap[timestamp];
+            const { restaurant } = newOrderDetail[timestamp] || {};
+            const { id: currentRestaurantId, menuId } = restaurant || {};
+
+            if (isAvailable) {
+              if (restaurant && currentRestaurantId && menuId) {
+                const menuListing = await fetchListing(menuId);
+
+                const newFoodList = await prepareMenuFoodList({
+                  menu: menuListing,
+                  timestamp,
+                  ...oldOrderMetadata,
+                });
+
+                newOrderDetail[timestamp].restaurant = {
+                  ...newOrderDetail[timestamp].restaurant,
+                  foodList: newFoodList,
+                };
+              }
+            } else {
+              const recommendOrderDetail =
+                await recommendRestaurantForSpecificDay({
+                  orderId: newOrderId,
+                  shouldCalculateDistance: !isCreatedByAdmin,
+                  timestamp: Number(timestamp),
+                  favoriteRestaurantIdList: [currentRestaurantId!],
+                });
+
+              const { hasNoRestaurants = false } =
+                recommendOrderDetail[timestamp] || {};
+
+              if (!hasNoRestaurants) {
+                newOrderDetail[timestamp] = recommendOrderDetail[timestamp];
+              }
+            }
+
+            return null;
+          }),
+        );
+      }
+
+      const newPlanResponse = await integrationSdk.listings.create({
+        title: `${generatedOrderTitle} - Plan week ${index + 1}`,
+        authorId: subAccountId,
+        state: EListingStates.published,
+        metadata: {
+          ...oldPlanMetadata,
+          viewed: false,
+          orderId: newOrderId,
+          orderDetail: newOrderDetail,
+        },
+      });
+      const [newPlan] = denormalisedResponseEntities(newPlanResponse);
+
+      return newPlan?.id?.uuid;
+    }),
+  );
+};
---
+++ src/pages/api/apiServices/order/reorder/index.service.ts
@@ -0,0 +1,156 @@
+import { DateTime } from 'luxon';
+
+import type { TDaySession } from '@components/CalendarDashboard/helpers/types';
+import { generateUncountableIdForOrder } from '@helpers/generateUncountableId';
+import { getInitMemberOrder } from '@pages/api/orders/[orderId]/plan/memberOrder.helper';
+import { denormalisedResponseEntities } from '@services/data';
+import { getOrderNumber, updateOrderNumber } from '@services/getAdminAccount';
+import { fetchUser } from '@services/integrationHelper';
+import { getIntegrationSdk } from '@services/integrationSdk';
+import { Listing, User } from '@src/utils/data';
+import {
+  formatTimestamp,
+  generateWeekDayList,
+  renderDateRange,
+} from '@src/utils/dates';
+import {
+  EBookerOrderDraftStates,
+  EListingStates,
+  EOrderDraftStates,
+  EOrderType,
+} from '@src/utils/enums';
+
+import { createNewPlanFromOldPlans, normalizeOrderMetadata } from './helper';
+
+export const reorder = async ({
+  orderIdToReOrder,
+  bookerId,
+  isCreatedByAdmin,
+  params,
+}: {
+  orderIdToReOrder: string;
+  bookerId: string;
+  isCreatedByAdmin?: boolean;
+  params: {
+    startDate: number;
+    endDate: number;
+    daySession?: TDaySession;
+  };
+}) => {
+  const integrationSdk = getIntegrationSdk();
+  const response = await integrationSdk.listings.show({
+    id: orderIdToReOrder,
+  });
+  const [oldOrder] = denormalisedResponseEntities(response);
+  const oldOrderMetadata = Listing(oldOrder).getMetadata();
+
+  const {
+    companyId,
+    plans: oldPlans = [],
+    orderType,
+    selectedGroups = [],
+    startDate: oldStartDate,
+    endDate: oldEndDate,
+  } = oldOrderMetadata;
+  const isGroupOrder = orderType === EOrderType.group;
+  const { startDate, endDate, daySession } = params;
+
+  const companyAccount = await fetchUser(companyId);
+  const currentOrderNumber = await getOrderNumber();
+  const companyGetter = User(companyAccount);
+  const { subAccountId } = companyGetter.getPrivateData();
+  const { companyName } = companyGetter.getPublicData();
+
+  const generatedOrderTitle = `PT${generateUncountableIdForOrder(
+    currentOrderNumber,
+  )}`;
+
+  // Prepare order state history
+  const orderState = isCreatedByAdmin
+    ? EOrderDraftStates.draft
+    : EBookerOrderDraftStates.bookerDraft;
+
+  const orderStateHistory = [
+    {
+      state: orderState,
+      updatedAt: new Date().getTime(),
+    },
+  ];
+
+  const newOrderResponse = await integrationSdk.listings.create(
+    {
+      authorId: subAccountId,
+      title: generatedOrderTitle,
+      state: EListingStates.published,
+      publicData: {
+        companyName,
+        orderName: `${companyName}_${formatTimestamp(
+          startDate,
+        )} - ${formatTimestamp(endDate)}`,
+      },
+      metadata: {
+        bookerId,
+        orderStateHistory,
+        orderState,
+        companyName,
+        ...normalizeOrderMetadata(oldOrderMetadata, {
+          daySession,
+        }),
+        startDate,
+        endDate,
+        deadlineDate: DateTime.fromMillis(startDate)
+          .setZone('Asia/Ho_Chi_Minh')
+          .minus({ day: 2 })
+          .toMillis(),
+      },
+    },
+    { expand: true },
+  );
+
+  // * new order date list info
+  const orderDatesInTimestamp = renderDateRange(startDate, endDate);
+  const orderDatesWeekdayList = generateWeekDayList(startDate, endDate);
+  // * old order date list info
+  const oldOrderDatesInTimestamp = renderDateRange(oldStartDate, oldEndDate);
+  const oldOrderDatesWeekdayList = generateWeekDayList(
+    oldStartDate,
+    oldEndDate,
+  );
+  const [newOrder] = denormalisedResponseEntities(newOrderResponse);
+  const newOrderId = newOrder?.id?.uuid;
+  const initialMemberOrder = getInitMemberOrder({
+    companyAccount,
+    selectedGroups,
+  });
+
+  const newPlanIds = await createNewPlanFromOldPlans({
+    newOrderId,
+    newOrder,
+    generatedOrderTitle,
+    subAccountId,
+    isCreatedByAdmin,
+    oldPlans,
+    integrationSdk,
+    orderDatesInTimestamp,
+    orderDatesWeekdayList,
+    oldOrderDatesInTimestamp,
+    oldOrderDatesWeekdayList,
+    isGroupOrder,
+    initialMemberOrder,
+    oldOrderMetadata,
+  });
+
+  const updatedOrder = await integrationSdk.listings.update(
+    {
+      id: newOrderId,
+      metadata: {
+        plans: newPlanIds,
+      },
+    },
+    { expand: true },
+  );
+
+  updateOrderNumber();
+
+  return denormalisedResponseEntities(updatedOrder)[0];
+};
