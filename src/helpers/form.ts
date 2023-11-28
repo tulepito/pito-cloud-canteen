aadf13121f3567f330d1b0b83e70a5e3e03b7bda
Anh Nguyen <anh.nguyen@gigarion.com>
2023-11-28 16:22:10 +0700

    update css for nutrition

--- src/helpers/form.ts
+++ src/helpers/form.ts
@@ -4,7 +4,21 @@ import type { TObject } from '@utils/types';
 
 const sortArrayInObject = (obj: TObject) => {
   if (typeof obj !== 'object') return obj;
-  if (Array.isArray(obj)) return obj.sort();
+
+  if (Array.isArray(obj)) {
+    if (obj.length > 0) {
+      try {
+        return Object.freeze(obj.sort());
+      } catch (error) {
+        // Code to handle the exception
+        console.error(`An error occurred: ${error}`);
+
+        return obj;
+      }
+    }
+
+    return obj;
+  }
 
   return Object.keys(obj).reduce((acc: TObject, key) => {
     acc[key] = sortArrayInObject(obj[key]);
