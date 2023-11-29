2d71af66fd6b8105798e02c157890768d4f85650
Allen <khang.nguyen@journeyh.io>
2023-11-29 10:44:47 +0700

    PCC-715 add: automatic picking food form

---
+++ src/components/OrderDetails/EditView/AutomaticInfoSection/AutomaticPickingForm.tsx
@@ -0,0 +1,73 @@
+import type { FormProps, FormRenderProps } from 'react-final-form';
+import { Field, Form as FinalForm } from 'react-final-form';
+import { OnChange } from 'react-final-form-listeners';
+
+import Form from '@components/Form/Form';
+import RenderWhen from '@components/RenderWhen/RenderWhen';
+import Toggle from '@components/Toggle/Toggle';
+
+import css from './AutomaticPickingForm.module.scss';
+
+export type TAutomaticPickingFormValues = {};
+
+type TExtraProps = {
+  handleFieldChange?: () => void;
+  shouldShowMainTitle?: boolean;
+};
+type TAutomaticPickingFormComponentProps =
+  FormRenderProps<TAutomaticPickingFormValues> & Partial<TExtraProps>;
+type TAutomaticPickingFormProps = FormProps<TAutomaticPickingFormValues> &
+  TExtraProps;
+
+const AutomaticPickingFormComponent: React.FC<
+  TAutomaticPickingFormComponentProps
+> = (props) => {
+  const {
+    handleFieldChange = console.info,
+    shouldShowMainTitle = true,
+    handleSubmit,
+  } = props;
+
+  const subTitle =
+    'Cho phép PITO Cloud Canteen tự chọn món cho những thành viên không xác nhận đặt đơn.';
+
+  const mainTitle = 'Tự động chọn món cho thành viên';
+
+  const titleComponent = (
+    <div className={css.titleContainer}>
+      <RenderWhen condition={shouldShowMainTitle}>
+        <div className={css.mainTitle}>{mainTitle}</div>
+      </RenderWhen>
+      <div>{subTitle}</div>
+    </div>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <OnChange name={'autoPicking'}>{handleFieldChange}</OnChange>
+      <div className={css.fieldContainer}>
+        {titleComponent}
+        <Field id="pickAllow" name="autoPicking">
+          {({ id, input }) => {
+            return (
+              <Toggle
+                id={id}
+                name={input.name}
+                status={input.value ? 'on' : 'off'}
+                onClick={(value) => {
+                  input.onChange(value);
+                }}
+              />
+            );
+          }}
+        </Field>
+      </div>
+    </Form>
+  );
+};
+
+const AutomaticPickingForm: React.FC<TAutomaticPickingFormProps> = (props) => {
+  return <FinalForm {...props} component={AutomaticPickingFormComponent} />;
+};
+
+export default AutomaticPickingForm;
