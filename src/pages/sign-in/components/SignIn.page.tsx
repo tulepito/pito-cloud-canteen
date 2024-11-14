45f18eb919db2d39e1a7b3b548b055726a929a4b
Lê Văn Tư <136409689+tulepito@users.noreply.github.com>
2024-11-14 13:30:27 +0700

    [stable] update/prevent-select-role-after-login (#33)

--- src/pages/sign-in/components/SignIn.page.tsx
+++ src/pages/sign-in/components/SignIn.page.tsx
@@ -3,20 +3,15 @@ import { FormattedMessage } from 'react-intl';
 import { useRouter } from 'next/router';
 
 import { useAppDispatch, useAppSelector } from '@hooks/reduxHooks';
-import { useRoleSelectModalController } from '@hooks/useRoleSelectModalController';
 import { authenticationInProgress, authThunks } from '@redux/slices/auth.slice';
 import { userThunks } from '@redux/slices/user.slice';
 import { enGeneralPaths } from '@src/paths';
-import { EUserSystemPermission } from '@src/utils/enums';
 
 import type { TSignInFormValues } from './SignInForm';
 import SignInForm from './SignInForm';
 
 const SignInPage = () => {
   const authInprogress = useAppSelector(authenticationInProgress);
-  const { onOpenRoleSelectModal } = useRoleSelectModalController();
-  const currentRole = useAppSelector((state) => state.user.currentRole);
-  const userPermission = useAppSelector((state) => state.user.userPermission);
   const { signInError, isAuthenticated } = useAppSelector(
     (state) => state.auth,
   );
@@ -39,12 +34,6 @@ const SignInPage = () => {
     await dispatch(userThunks.fetchCurrentUser());
     await dispatch(authThunks.authInfo());
   };
-
-  useEffect(() => {
-    if (!currentRole && userPermission === EUserSystemPermission.company) {
-      onOpenRoleSelectModal();
-    }
-  }, [currentRole, onOpenRoleSelectModal, userPermission]);
 
   useEffect(() => {
     if (isAuthenticated) {
