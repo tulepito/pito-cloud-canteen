b654cc76fd2da20c315f1d7e25c8bb81e25793db
thaipito <149988278+thaipito@users.noreply.github.com>
2025-07-02 16:40:23 +0700

    chore: disable check inject gleap (#161)
    Co-authored-by: Ivantime <ivantime000@gmail.com>

--- src/components/UIProvider/GleapCSSInjector.tsx
+++ src/components/UIProvider/GleapCSSInjector.tsx
@@ -1,28 +1,6 @@
 import { useEffect } from 'react';
 
-import { checkRoleOfCurrentUser } from '@helpers/auth';
-import { useAppSelector } from '@hooks/reduxHooks';
-import { ECompanyPermission } from '@src/utils/enums';
-
 function GleapCSSInjector() {
-  const currentUser = useAppSelector((state) => state.user.currentUser);
-
-  useEffect(() => {
-    if (
-      !checkRoleOfCurrentUser(currentUser, [
-        ECompanyPermission.owner,
-        ECompanyPermission.booker,
-        ECompanyPermission.participant,
-      ])
-    ) {
-      document.body.classList.add('no-gleap');
-    } else {
-      document.body.classList.remove('no-gleap');
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentUser?.id?.uuid]);
-
   useEffect(() => {
     if (window.innerWidth < 768) {
       document.body.classList.add('mobile-gleap');
