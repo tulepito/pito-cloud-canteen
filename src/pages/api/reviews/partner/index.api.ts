b84e4de659b2c69254481a72db3f5e4b75d6adf2
Hao Nguyen Van <vanhao.nguyen@pito.vn>
2025-11-14 10:45:57 +0700

    update: build fullname of reviewer on review module

--- src/pages/api/reviews/partner/index.api.ts
+++ src/pages/api/reviews/partner/index.api.ts
@@ -4,9 +4,10 @@ import { HttpMethod } from '@apis/configs';
 import cookies from '@services/cookie';
 import { denormalisedResponseEntities } from '@services/data';
 import partnerChecker from '@services/permissionChecker/partner';
-import { getSdk, handleError } from '@services/sdk';
+import { getIntegrationSdk, getSdk, handleError } from '@services/sdk';
 import type { RatingListing } from '@src/types';
 import { CurrentUser } from '@src/utils/data';
+import { buildFullNameFromProfile } from '@src/utils/emailTemplate/participantOrderPicking';
 import { EListingType } from '@src/utils/enums';
 import { SuccessResponse } from '@src/utils/response';
 import type { TPagination } from '@src/utils/types';
@@ -15,6 +16,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const apiMethod = req.method;
   try {
     const sdk = getSdk(req, res);
+    const integrationSdk = getIntegrationSdk();
 
     switch (apiMethod) {
       case HttpMethod.GET: {
@@ -53,14 +55,19 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             reviews.map(async (review) => {
               const metadata = review.attributes?.metadata;
               const authorId = metadata?.reviewerId;
-              const author = await sdk.users.show({
+              const author = await integrationSdk.users.show({
                 id: authorId as string,
+                include: ['profileImage'],
               });
               const [authorData] = denormalisedResponseEntities(author);
 
+              const fullName = buildFullNameFromProfile(
+                authorData.attributes.profile,
+              );
+
               return {
                 ...review,
-                authorName: authorData.attributes.profile.displayName,
+                authorName: fullName,
               };
             }),
           );
