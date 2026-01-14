2e3668bfa185bab7eedd2fc0999beac0705aa065
Hao Nguyen Van <vanhao.nguyen@pito.vn>
2026-01-14 15:54:50 +0700

    update: sync restaurant name to owner name when editing restaurant info (#273)

--- src/pages/api/admin/listings/restaurant/update.api.ts
+++ src/pages/api/admin/listings/restaurant/update.api.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import cookies from '@services/cookie';
+import { denormalisedResponseEntities } from '@services/data';
 import { fetchListing } from '@services/integrationHelper';
 import { createNativeNotificationToPartner } from '@services/nativeNotification';
 import { createFirebaseDocNotification } from '@services/notifications';
@@ -11,11 +12,36 @@ import { ENativeNotificationType, ENotificationType } from '@src/utils/enums';
 async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   try {
     const { dataParams, queryParams = {} } = req.body;
+    if (!dataParams.title || !dataParams.id) {
+      return res
+        .status(400)
+        .json({ error: 'Restaurant name and ID are required' });
+    }
+    const newRestaurantName = dataParams.title;
+    const restaurantId = dataParams.id;
     const integrationSdk = getIntegrationSdk();
     const response = await integrationSdk.listings.update(
       dataParams,
       queryParams,
     );
+    // sync restaurant name to restaurant owner
+    const restaurantOwnerResponse = await integrationSdk.users.query({
+      meta_restaurantListingId: restaurantId,
+      meta_isPartner: true,
+      perPage: 1,
+    });
+    const [restaurantOwner] = denormalisedResponseEntities(
+      restaurantOwnerResponse,
+    );
+    const restaurantOwnerId = restaurantOwner?.id?.uuid;
+    if (restaurantOwnerId) {
+      await integrationSdk.users.updateProfile({
+        id: restaurantOwnerId,
+        displayName: newRestaurantName,
+        lastName: newRestaurantName.split(' ')[0],
+        firstName: newRestaurantName.split(' ').slice(1).join(' '),
+      });
+    }
 
     const restaurant = await fetchListing(dataParams?.id, ['author']);
     createFirebaseDocNotification(
